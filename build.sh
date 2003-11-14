9a924988e2e48fb2596d0b3fc80a190163b4e399
rubino <rubino@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2003-11-14 10:10:59 +0000

    first step to a build env

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# module add jdk-1.4.1-sun
+source envs.sh
+cd de/unika/ipd/grgen/parser/
+java antlr.Tool grgen.g
+cd ../../../../..
+find . -type f -name "*.java" | xargs javac -source 1.4
