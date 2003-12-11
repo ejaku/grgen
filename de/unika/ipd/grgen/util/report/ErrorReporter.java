046f9ed8c91c8baf598659defa6f6b36f1fc6ccc
beck <beck@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2003-12-11 10:12:32 +0000

    Counts errors and warnings now

--- de/unika/ipd/grgen/util/report/ErrorReporter.java
+++ de/unika/ipd/grgen/util/report/ErrorReporter.java
@@ -5,13 +5,16 @@
 package de.unika.ipd.grgen.util.report;
 
 /**
- * 
+ * The error reported class.
  */
 public class ErrorReporter extends Reporter {
 
 	public final static int ERROR = 0;
 	public final static int WARNING = 1;
 	public final static int NOTE = 2;
+	
+	protected static int errCount = 0; 
+	protected static int warnCount = 0; 
 
 	private final static String[] levelNames = {
 		"error", "warning", "note"
@@ -29,27 +32,60 @@ public class ErrorReporter extends Reporter {
   }
   
   
-  
+  /**
+   * Report an error at a given location.
+   * 
+   * @param loc The location.
+   * @param msg The error message.
+   */
   public void error(Location loc, String msg) {
   	report(ERROR, loc, getMsg(ERROR, msg));
+  	++errCount;
   }
   
+  /**
+   * Report an error.
+   * @param msg
+   */
   public void error(String msg) {
   	report(ERROR, getMsg(ERROR, msg));
+	  ++errCount;
   }
   
+  /**
+   * Report a warning at a given location.
+   * 
+   * @param loc The location.
+   * @param msg The warning message.
+   */
   public void warning(Location loc, String msg) {
   	report(WARNING, getMsg(WARNING, msg));
+		++warnCount;
   }
-  
+
+	/**
+	 * report a warning.
+   * @param msg The warning message.
+	 */  
   public void warning(String msg) {
   	report(WARNING, getMsg(WARNING, msg));
+  	++warnCount;
   }
-  
+
+	/**
+	 * Report a note at a given location.
+	 * 
+   * @param loc The location.
+   * @param msg The note message.
+	 */  
   public void note(Location loc, String msg) {
   	report(NOTE, loc, getMsg(NOTE, msg));
   }
   
+  /**
+   * Report a note.
+   * @param msg The note message.
+   */
   public void note(String msg) {
   	report(NOTE, getMsg(NOTE, msg));
   }
@@ -66,4 +102,21 @@ public class ErrorReporter extends Reporter {
    */
   public void leaving() {
   }
+  
+  /**
+   * Returns the number of occured errors.
+   * @return
+   */
+  public int getErrorCount() {
+	return errCount;
+  }
+
+  /**
+   * Returns the number of occured warnings.
+   * @return
+   */
+  public int getWarnCount() {
+	return warnCount;
+  }
+  
 }
