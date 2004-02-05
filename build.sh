a87463db590eaf9057aa511d65552abf0cef203e
rubino <rubino@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2004-02-05 16:42:51 +0000

    initial Makefile removed useless files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# module add jdk-1.4.1-sun
-source envs.sh
-cd de/unika/ipd/grgen/parser/antlr/
-java antlr.Tool grgen.g
-cd ../../../../..
-find . -type f -name "*.java" | xargs javac -source 1.4
