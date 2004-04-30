65149e34b8c0436452f49e1f82d96b2b87b20e51
rubino <rubino@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2004-04-30 12:42:13 +0000

    build ast for connect assertion

---
+++ de/unika/ipd/grgen/ast/ConnAssertNode.java
@@ -0,0 +1,84 @@
+/**
+ * @author shack
+ * @version $Id$
+ */
+package de.unika.ipd.grgen.ast;
+
+
+
+import de.unika.ipd.grgen.ast.util.*;
+
+import de.unika.ipd.grgen.ir.Graph;
+import java.util.Set;
+
+/**
+ * Node that represents a Connection
+ * Children are: Node, Edge, Node
+ */
+public class ConnAssertNode extends BaseNode {
+	
+	static {
+		setName(ConnAssertNode.class, "conn_assert");
+	}
+	
+	/** edge names for the children. */
+	private static final String[] childrenNames = {
+		"edge", "range", "node",
+	};
+	
+	/** Index of the source node. */
+	private static final int EDGE = 0;
+	
+	/** Index of the edge node. */
+	private static final int RANGE = 1;
+	
+	/** Index of the target node. */
+	private static final int NODE= 2;
+	
+	/** Resolver for the nodes. */
+	
+	private static final Resolver nodeResolver =
+		new OptionalResolver(new DeclResolver(NodeDeclNode.class));
+	
+	private static final Checker nodeChecker =
+		new MultChecker(new Class[] {
+				NodeDeclNode.class, NodeTypeChangeNode.class
+			});
+	
+	private boolean outgoing;
+	
+	/**
+	 * Construct a new connection assertion node.
+	 * Graphically: hostnode -> edge [range] -> node
+	 * @param edge The edge type connected to the host node.
+	 * @param range The allowed number of edges of one type connected to the host node.
+	 * @param node The node the host node is connected to, via the edge.
+	 * @param outgoing true, if the edge is a out going one.
+	 */
+	public ConnAssertNode(BaseNode edge, BaseNode range,
+						  BaseNode node, boolean outgoing) {
+		super(edge.getCoords());
+		addChild(edge);
+		addChild(range);
+		addChild(node);
+		this.outgoing = outgoing;
+		setChildrenNames(childrenNames);
+		addResolver(EDGE, new EdgeResolver(getScope(), edge.getCoords(), false));
+		addResolver(RANGE, null); // TODO range resolver
+		addResolver(NODE, nodeResolver);
+	}
+	
+	/**
+	 * Check, if the AST node is correctly built.
+	 * @see de.unika.ipd.grgen.ast.BaseNode#check()
+	 */
+	protected boolean check() {
+		return checkChild(EDGE, EdgeCharacter.class)
+			&& checkChild(RANGE, RangeSpecNode.class)
+			&& checkChild(NODE, NodeCharacter.class);
+	}
+	
+	public boolean isOutgoing() {
+		return outgoing;
+	}
+}
