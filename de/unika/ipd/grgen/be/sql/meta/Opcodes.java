9850cbb88a1830b06526068acd068b32c9c294c5
shack <shack@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2004-07-12 08:36:27 +0000

    Niceified the constants.

--- de/unika/ipd/grgen/be/sql/meta/Opcodes.java
+++ de/unika/ipd/grgen/be/sql/meta/Opcodes.java
@@ -14,37 +14,37 @@ public interface Opcodes {
 	
 	int INVALID = -1;
 	int MUL = 0;
-	int DIV = 1;
-	int MOD = 2;
-	int ADD = 3;
-	int SUB = 4;
-	int NEG = 5;
+	int DIV = MUL + 1;
+	int MOD = DIV + 1;
+	int ADD = MOD + 1;
+	int SUB = ADD + 1;
+	int NEG = SUB + 1;
 	
-	int EQ = 6;
-	int NE = 7;
-	int LT = 8;
-	int LE = 9;
-	int GT = 10;
-	int GE = 11;
+	int EQ = NEG + 1;
+	int NE = EQ + 1;
+	int LT = NE + 1;
+	int LE = LT + 1;
+	int GT = LE + 1;
+	int GE = GT + 1;
 
-	int BETWEEN_AND = 12;
-	int SET_IN = 13;
-	int SUBQUERY_IN = 14;
+	int BETWEEN_AND = GE + 1;
+	int SET_IN = BETWEEN_AND + 1;
+	int SUBQUERY_IN = SET_IN + 1;
 	
-	int NOT = 15;
-	int AND = 16;
-	int OR = 17;
+	int NOT = SUBQUERY_IN + 1;
+	int AND = NOT + 1;
+	int OR = AND + 1;
 	
-	int BIT_AND = 18;
-	int BIT_OR = 19;
-	int BIT_XOR = 20;
-	int BIT_NOT = 21;
-	int SHL = 22;
-	int SHR = 23;
+	int BIT_AND = OR + 1;
+	int BIT_OR = BIT_AND + 1;
+	int BIT_XOR = BIT_OR + 1;
+	int BIT_NOT = BIT_XOR + 1;
+	int SHL = BIT_NOT + 1;
+	int SHR = SHL + 1;
 	
-	int COND = 24;
-	int EXISTS = 25;
-	int ISNULL = 26;
+	int COND = SHR + 1;
+	int EXISTS = COND + 1;
+	int ISNULL = EXISTS + 1;
 	
 }
 
