1f6dfdce81bfab0ef7c40cfe58cb54ef303f2882
rubino <rubino@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2005-10-10 13:17:27 +0000

    LGPL

--- run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-FLAGS="-a -i "
-BE=de.unika.ipd.grgen.be.C.PGSQLBackend
-OUTDIR=$2
-INFILE=$1
-shift 2
-
-java -ea de.unika.ipd.grgen.Main $FLAGS -b $BE -p settings.xml $* -o $OUTDIR $INFILE 
