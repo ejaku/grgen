9bdef228833358671c355a06cd5bf3cf2f22ef9b
buchwald <buchwald@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2007-12-12 10:50:52 +0000

    Added new AST-nodes for dpo, exact and induced statements.

---
+++ frontend/de/unika/ipd/grgen/ast/ExactNode.java
@@ -0,0 +1,79 @@
+/*
+  GrGen: graph rewrite generator tool.
+  Copyright (C) 2005  IPD Goos, Universit"at Karlsruhe, Germany
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * @author buchwald
+ * @version $Id: $
+ */
+package de.unika.ipd.grgen.ast;
+
+import java.awt.Color;
+
+import de.unika.ipd.grgen.ast.util.DeclResolver;
+import de.unika.ipd.grgen.ast.util.Resolver;
+import de.unika.ipd.grgen.parser.Coords;
+
+public class ExactNode extends BaseNode {
+
+	static {
+		setName(ExactNode.class, "exact");
+	}
+
+	public ExactNode(Coords coords) {
+		super(coords);
+	}
+
+	/**
+	 * Check whether all children are of node type.
+	 * 
+	 * TODO warn if some statements are redundant.
+	 */
+	@Override
+	protected boolean check() {
+		if (getChildren().isEmpty()) {
+			this.reportError("Exact statement is empty");
+			return false;
+		}
+
+		return checkAllChildren(NodeDeclNode.class);
+	}
+
+	public Color getNodeColor() {
+		return Color.PINK;
+	}
+
+	/**
+	 * @see de.unika.ipd.grgen.ast.BaseNode#resolve()
+	 */
+	@Override
+	protected boolean resolve() {
+		boolean resolved = true;
+
+		Resolver resolver = new DeclResolver(new Class[] { NodeDeclNode.class });
+
+		for (int i = 0; i < children(); ++i) {
+			boolean res = resolver.resolve(this, i);
+			if (!res)
+				resolved = false;
+		}
+
+		return resolved;
+	}
+
+}
