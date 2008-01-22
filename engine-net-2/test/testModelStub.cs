c13f2a5ed4be5bbb8701f056d43526b80f00e32a
moritz <moritz@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-01-22 14:48:47 +0000

    Added missing test file

---
+++ engine-net-2/test/testModelStub.cs
@@ -0,0 +1,80 @@
+// This file has been generated automatically by GrGen.
+// Do not modify this file! Any changes will be lost!
+// Rename this file or use a copy!
+// Generated from "test.grg" on Wed Dec 19 13:46:54 CET 2007
+
+using System;
+using System.Collections.Generic;
+using de.unika.ipd.grGen.libGr;
+using de.unika.ipd.grGen.lgsp;
+using de.unika.ipd.grGen.models.test;
+
+namespace test
+{
+	public class D231_4121_Impl : Node_D231_4121
+	{
+		public D231_4121_Impl() : base() { }
+
+		public override INode Clone() { return new D231_4121_Impl(this); }
+
+		private D231_4121_Impl(D231_4121_Impl oldElem) : base()
+		{
+			_d231_4121 = oldElem._d231_4121;
+			_a2 = oldElem._a2;
+			_b23 = oldElem._b23;
+			_a4 = oldElem._a4;
+			_b41 = oldElem._b41;
+			_b42 = oldElem._b42;
+			_a5 = oldElem._a5;
+		}
+
+		private int _d231_4121;
+		public override int @d231_4121
+		{
+			get { return _d231_4121; }
+			set { _d231_4121 = value; }
+		}
+
+		private int _a2;
+		public override int @a2
+		{
+			get { return _a2; }
+			set { _a2 = value; }
+		}
+
+		private int _b23;
+		public override int @b23
+		{
+			get { return _b23; }
+			set { _b23 = value; }
+		}
+
+		private int _a4;
+		public override int @a4
+		{
+			get { return _a4; }
+			set { _a4 = value; }
+		}
+
+		private int _b41;
+		public override int @b41
+		{
+			get { return _b41; }
+			set { _b41 = value; }
+		}
+
+		private int _b42;
+		public override int @b42
+		{
+			get { return _b42; }
+			set { _b42 = value; }
+		}
+
+		private int _a5;
+		public override int @a5
+		{
+			get { return _a5; }
+			set { _a5 = value; }
+		}
+	}
+}
