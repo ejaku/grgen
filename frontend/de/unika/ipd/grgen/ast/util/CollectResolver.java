b31ce14580217f64540f67bb20a4dd715a225246
buchwald <buchwald@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-01-28 16:41:36 +0000

    forgot the collectResolver.

---
+++ frontend/de/unika/ipd/grgen/ast/util/CollectResolver.java
@@ -0,0 +1,51 @@
+/*
+ GrGen: graph rewrite generator tool.
+ Copyright (C) 2007  IPD Goos, Universit"at Karlsruhe, Germany
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+package de.unika.ipd.grgen.ast.util;
+
+import de.unika.ipd.grgen.ast.BaseNode;
+import de.unika.ipd.grgen.ast.GenCollectNode;
+
+/**
+ * A resolver, that resolves a source AST CollectNode into a target AST CollectNode of type T,
+ * by using a given resolver. 
+ */
+public class CollectResolver<T extends BaseNode>
+{
+	private GenResolver<T> resolver;
+	
+	public CollectResolver(GenResolver<T> resolver) {
+		this.resolver = resolver;
+	}
+	
+	/** resolves n to node of type R, via declaration if n is an identifier, via simple cast otherwise 
+	 *  returns null if n's declaration or n can't be cast to R */
+	public GenCollectNode<T> resolve(GenCollectNode<?> collect) {
+		GenCollectNode<T> res = new GenCollectNode<T>();
+		for (BaseNode elem : collect.getChildren()) {
+	        T resolved = resolver.resolve(elem, collect);
+	        if (resolved == null) {
+	        	return null;
+	        }
+	        res.addChild(resolved);
+        }
+		return res;
+	}
+}
