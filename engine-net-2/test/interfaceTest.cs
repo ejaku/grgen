0196b522db94680b5d22f3b37d1bfeba77b826c0
moritz <moritz@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-01-30 15:32:54 +0000

    Mono bug testcase...

---
+++ engine-net-2/test/interfaceTest.cs
@@ -0,0 +1,73 @@
+﻿//#define TEST_FIRST
+
+interface IA
+{
+	string val { get; set; }
+}
+
+interface IAI : IA
+{
+	new int val { get; set; }
+}
+
+#if TEST_FIRST
+
+class AI : IAI
+{
+	public int val { get { return 42; } set {} }
+	string IA.val { get { return "13"; } set {} }
+
+	public void stuff(AI other)
+	{
+		int x = 7;
+		other.val = x;
+		val = x;
+		x = other.val;
+		x = val;
+		val = other.val;
+	}
+
+	public void stuff2(IAI other)
+	{
+		int x = 7;
+		other.val = x;
+		val = x;
+		x = other.val;
+		x = val;
+		val = other.val;
+	}
+}
+
+#else
+
+interface IAI2 : IAI { }
+
+class AI2 : IAI2
+{
+	public int val { get { return 42; } set { } }
+	string IA.val { get { return "13"; } set { } }
+
+	public void stuff(AI2 other)
+	{
+		int x = 7;
+		other.val = x;
+		val = x;
+		x = other.val;
+		x = val;
+		val = other.val;
+	}
+
+	public void stuff2(IAI2 other)
+	{
+		int x = 7;
+		other.val = x;
+		val = x;
+		x = other.val;
+		x = val;
+		val = other.val;
+	}
+}
+
+#endif
+
+class MainClass { public static void Main() { } }
