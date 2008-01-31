54555d521aab500a8d394e772d51b3c85ce87c6d
rubino <rubino@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-01-31 19:46:39 +0000

    the master test.sh is in place, now

---
+++ frontend/test/test.sh
@@ -0,0 +1,93 @@
+#! /bin/sh
+
+trap "echo; exit 1" INT QUIT HUP TERM
+
+GRGENDIR=".."
+JARGS="$GRGENDIR/jars/jargs.jar"
+ANTLR="$GRGENDIR/jars/antlr.jar"
+[ "$GRGENNET" ] || GRGENNET="$GRGENDIR/../engine-net-2/out/bin/"
+BE_CSC=de.unika.ipd.grgen.be.Csharp.SearchPlanBackend2
+
+
+GOLD=summary_gold.log
+LOG=summary.log
+TESTS="should_pass/*.grg should_warn/*.grg should_fail/*.grg"
+
+OUTPUTSUFF=_out
+
+APPEND=""
+ONLY_NEW=""
+VERBOSE=""
+
+do_diff()
+{
+	diff -U0 $GOLD $LOG | grep "^\+"
+}
+
+while [ "$1" ]; do
+	case "$1" in
+		--) shift; break;;
+		-a) shift; APPEND="TRUE";;
+		-c) rm -fr */*$OUTPUTSUFF; exit 0;;
+		-d) do_diff; exit 0;;
+		-n) shift; ONLY_NEW="TRUE";;
+		-v) shift; VERBOSE="TRUE";;
+		* ) break;;
+	esac
+done
+
+[ "$APPEND" ] || rm -f $LOG
+touch $LOG
+
+if uname -s | grep -iq "cygwin"; then
+	SEP=";"
+else
+	SEP=":"
+fi
+CLASSPATH=$JARGS$SEP$ANTLR$SEP$GRGENNET/grgen.jar
+
+JAVA_ARGS="-Xmx256M -cp $CLASSPATH -ea de.unika.ipd.grgen.Main -b $BE_CSC"
+
+do_test()
+{
+	local FILE="$1"
+	local DIR="`echo "$FILE" | sed -e s/\\.grg\$/$OUTPUTSUFF/`"
+	if [ "$ONLY_NEW" -a "$DIR" -nt "$FILE" ]; then return 0; fi
+	rm -fr -- "$DIR"
+	mkdir  -- "$DIR"
+	echo -n "===> TEST $FILE"
+	if java $JAVA_ARGS -o "$DIR" "$FILE" > "$DIR/log" 2>&1; then
+		echo -n " ... OK"
+		if mono "$GRGENNET/GrGen.exe" -keep -use "$DIR" -o "$DIR" "$FILE" >> "$DIR/log" 2>&1; then
+			if grep -q "WARNING" < "$DIR/log"; then
+				echo " ... WARNED"
+				echo "WARNED $FILE" >> "$LOG"
+			else
+				echo " ... OK(C#)"
+				echo "OK     $FILE" >> "$LOG"
+			fi
+		else
+			echo " ... FAILED(C#)"
+			echo "FAILED $FILE" >> "$LOG"
+		fi
+	elif grep -q -v "ERROR\|WARNING" < "$DIR/log"; then
+		echo " ... ABEND"
+		echo "ABEND  $FILE" >> "$LOG"
+	else
+		echo " ... ERROR"
+		echo "ERROR  $FILE" >> "$LOG"
+	fi
+	if [ "$VERBOSE" ]; then cat "$DIR/log"; fi
+}
+
+if [ "$1" ]; then
+	for i in "$@";  do
+		if echo "$i" | grep -q "\\.grg\$"; then
+			do_test "$i"; 
+		fi
+	done
+else
+	for i in $TESTS; do do_test "$i"; done
+fi
+
+do_diff
