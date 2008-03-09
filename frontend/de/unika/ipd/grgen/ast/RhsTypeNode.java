427e65e134303f747fdb70ecdce5b2ed82690318
buchwald <buchwald@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-03-09 22:44:52 +0000

    forgot RhsTypeNode

---
+++ frontend/de/unika/ipd/grgen/ast/RhsTypeNode.java
@@ -0,0 +1,48 @@
+/*
+ GrGen: graph rewrite generator tool.
+ Copyright (C) 2007  IPD Goos, Universit"at Karlsruhe, Germany
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package de.unika.ipd.grgen.ast;
+
+import java.util.Collection;
+import java.util.Vector;
+
+
+/**
+ * Type of right-hand side decl node declaration.
+ */
+class RhsTypeNode extends TypeNode {
+	static {
+		setName(RhsTypeNode.class, "right-hand side type");
+	}
+
+	/** returns children of this node */
+	public Collection<BaseNode> getChildren() {
+		Vector<BaseNode> children = new Vector<BaseNode>();
+		// no children
+		return children;
+	}
+
+	/** returns names of the children, same order as in getChildren */
+	public Collection<String> getChildrenNames() {
+		Vector<String> childrenNames = new Vector<String>();
+		// no children
+		return childrenNames;
+	}
+};
+
