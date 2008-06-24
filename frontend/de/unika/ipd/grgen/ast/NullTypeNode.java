e7217b063fd3a20170a9a15e986e29bb5772de77
moritz <moritz@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-06-24 12:48:56 +0000

    Added missing file

---
+++ frontend/de/unika/ipd/grgen/ast/NullTypeNode.java
@@ -0,0 +1,41 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET v2 beta
+ * Copyright (C) 2008 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos
+ * licensed under GPL v3 (see LICENSE.txt included in the packaging of this file)
+ */
+
+/**
+ * @author Moritz Kroll
+ * @version $Id$
+ */
+package de.unika.ipd.grgen.ast;
+
+import de.unika.ipd.grgen.ir.IR;
+import de.unika.ipd.grgen.ir.ObjectType;
+
+public class NullTypeNode extends BasicTypeNode
+{
+	static {
+		setName(NullTypeNode.class, "null type");
+	}
+
+	public boolean isCompatibleTo(TypeNode t) {
+		// null is compatible to all graph element types, object and string
+		if(!(t instanceof BasicTypeNode)) return true;
+		if(t == BasicTypeNode.objectType || t == BasicTypeNode.stringType) return true;
+		return false;
+	}
+
+	public boolean isCastableTo(TypeNode t) {
+		return isCompatibleTo(t);
+	}
+
+	protected IR constructIR() {
+		// TODO: Check whether this is OK
+		return new ObjectType(getIdentNode().getIdent());
+	}
+
+	public String toString() {
+		return "null";
+	}
+}
