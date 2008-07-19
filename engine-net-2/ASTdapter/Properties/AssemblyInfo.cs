e2e67141d112ad4b4d03ce055dd363b7a3fbe8f3
eja <eja@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2008-07-19 18:09:03 +0000

    run of change file headers with assembly info rewriting

--- engine-net-2/ASTdapter/Properties/AssemblyInfo.cs
+++ engine-net-2/ASTdapter/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.20552.0")]
+[assembly: AssemblyVersion("2.0.20554.0")]
 [assembly: AssemblyInformationalVersionAttribute("GrGen.NET 2.0")]
-[assembly: AssemblyFileVersion("2.0.20552.0")]
+[assembly: AssemblyFileVersion("2.0.20554.0")]
