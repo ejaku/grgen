26dfcd01e6890c2e41178c9039ae02bc647ecd46
eja <eja@3d89f09a-1708-48ca-8d12-22ea5d92806f>
2009-06-25 21:38:32 +0000

    (hopefully) adapted make release scripts to new folder structure

--- make_betabins.sh
+++ make_betabins.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#this script is used by make_release to build the binary distribution and stand alone to build beta only binary distributions
 
 GRGENDIR=GrGenNET-V$1-`date +"%F"`
-GRGENDIRSRC=$GRGENDIR-src
 
 # export binaries and examples
-svn export https://pp.info.uni-karlsruhe.de/svn/firm/trunk/grgen/engine-net-2/out/ $GRGENDIR
+svn export https://pp.info.uni-karlsruhe.de/svn/firm/trunk/grgen/engine-net-2 $GRGENDIR
+	#keep bin, examples, examples-api,tests
+rm -rf src
+rm -rf tools
+
 svn cat https://pp.info.uni-karlsruhe.de/svn/firm/trunk/grgen/LICENSE.txt > $GRGENDIR/LICENSE.txt
 
 rm -rf $GRGENDIR/examples/UML
--- make_release.sh
+++ make_release.sh
@@ -9,33 +9,28 @@ svn export https://pp.info.uni-karlsruhe.de/svn/firm/trunk/grgen $GRGENDIRSRC
 # delete doc-sources
 mv $GRGENDIRSRC/doc/grgen.pdf $GRGENDIRSRC/
 mv $GRGENDIRSRC/doc/VeryShortIntroductionToVersion2.txt $GRGENDIRSRC/
+mv $GRGENDIRSRC/doc/ChangeLog.txt $GRGENDIRSRC/
+mv $GRGENDIRSRC/doc/README.txt $GRGENDIRSRC/
 rm -rf $GRGENDIRSRC/doc
 mkdir $GRGENDIRSRC/doc
 mv $GRGENDIRSRC/grgen.pdf $GRGENDIRSRC/doc/grgen.pdf
 mv $GRGENDIRSRC/VeryShortIntroductionToVersion2.txt $GRGENDIRSRC/doc/VeryShortIntroductionToVersion2.txt
+mv $GRGENDIRSRC/ChangeLog.txt $GRGENDIRSRC/doc/ChangeLog.txt
+mv $GRGENDIRSRC/README.txt $GRGENDIRSRC/doc/README.txt
 
 rm $GRGENDIRSRC/make_release.sh
 rm $GRGENDIRSRC/make_betabins.sh
 rm -rf $GRGENDIRSRC/todo
 
-rm -rf $GRGENDIRSRC/engine-net-2/ChangeFileHeaders
-rm -rf $GRGENDIRSRC/engine-net-2/test
-rm -rf $GRGENDIRSRC/engine-net-2/out/examples/UML
-rm -rf $GRGENDIRSRC/engine-net-2/out/examples/Firm-IFConv
+rm -rf $GRGENDIRSRC/engine-net-2/tools/ChangeFileHeaders
+rm -rf $GRGENDIRSRC/engine-net-2/tools/test
+rm -rf $GRGENDIRSRC/engine-net-2/examples/UML
+rm -rf $GRGENDIRSRC/engine-net-2/examples/Firm-IFConv
 
 # make tar
 tar cjf $GRGENDIRSRC.tar.bz2 $GRGENDIRSRC
 zip -r $GRGENDIRSRC.zip $GRGENDIRSRC
 
 
-# export binaries and examples
-svn export https://pp.info.uni-karlsruhe.de/svn/firm/trunk/grgen/engine-net-2/out/ $GRGENDIR
-
-cp $GRGENDIRSRC/LICENSE.txt $GRGENDIR/
-
-rm -rf $GRGENDIR/examples/UML
-rm -rf $GRGENDIR/examples/Firm-IFConv
-
-# make tar
-tar cjf $GRGENDIR.tar.bz2 $GRGENDIR
-zip -r $GRGENDIR.zip $GRGENDIR
+#make binary distribution
+bash make_betabins.sh $1
