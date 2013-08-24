a6787b793e8314afdf89dfacbc758dd77a1159a3
eja <eja@localhost>
2013-08-24 13:07:52 +0200

    added function/procedure/method example (as development helper for methods/functions/procedures)

---
+++ engine-net-2/examples-api/FunctionsProceduresExample/FunctionsProceduresExample.cs
@@ -0,0 +1,49 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 4.0
+ * Copyright (C) 2003-2013 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+using System;
+using de.unika.ipd.grGen.lgsp;
+using de.unika.ipd.grGen.libGr;
+using de.unika.ipd.grGen.Action_FunctionsProceduresExample;
+using de.unika.ipd.grGen.Model_FunctionsProceduresExample;
+
+namespace FPE
+{
+    class FPEExample
+    {
+        FunctionsProceduresExampleGraph graph;
+        FunctionsProceduresExampleActions actions;
+        LGSPGraphProcessingEnvironment procEnv;
+
+        void DoFPE()
+        {
+            graph = new FunctionsProceduresExampleGraph();
+            actions = new FunctionsProceduresExampleActions(graph);
+            procEnv = new LGSPGraphProcessingEnvironment(graph, actions);
+
+            procEnv.PerformanceInfo = new PerformanceInfo();
+
+            // use graph rewrite sequence
+            procEnv.ApplyGraphRewriteSequence("init");
+
+            Console.WriteLine(procEnv.PerformanceInfo.MatchesFound + " matches found.");
+            Console.WriteLine(procEnv.PerformanceInfo.RewritesPerformed + " rewrites performed.");
+            procEnv.PerformanceInfo.Reset();
+
+            // use new 2.5 exact interface
+            IMatchesExact<Rule_r.IMatch_r> matchesExact = actions.r.Match(procEnv, 0);
+            Console.WriteLine(matchesExact.Count + " matches found.");
+            actions.r.Modify(procEnv, matchesExact.FirstExact);
+        }
+
+        static void Main(string[] args)
+        {
+            FPEExample eae = new FPEExample();
+            eae.DoFPE();
+        }
+    }
+}
