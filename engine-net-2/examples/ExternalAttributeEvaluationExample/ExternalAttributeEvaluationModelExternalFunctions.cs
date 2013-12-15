100bfa85a6d5f214d7a82348c3e95eb98ac19e8e
eja <eja@localhost>
2013-12-15 22:34:26 +0100

    adapted examples to filter function changes

--- engine-net-2/examples/ExternalAttributeEvaluationExample/ExternalAttributeEvaluationModelExternalFunctions.cs
+++ engine-net-2/examples/ExternalAttributeEvaluationExample/ExternalAttributeEvaluationModelExternalFunctions.cs
@@ -1,6 +1,6 @@
 // This file has been generated automatically by GrGen (www.grgen.net)
 // Do not modify this file! Any changes will be lost!
-// Generated from "ExternalAttributeEvaluation.grg" on Sat Sep 07 01:14:25 CEST 2013
+// Generated from "ExternalAttributeEvaluation.grg" on Sun Dec 15 18:55:24 CET 2013
 
 using System;
 using System.Collections.Generic;
