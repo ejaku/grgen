84241595f3f7eea81f3e76b9de95b1e6f671957d
eja <eja@localhost>
2014-03-09 14:28:55 +0100

    added assignment to change the name of a node/edge/(sub)graph from rule language:  - with syntax nameof(node/edge) = expression you can reassign/assign a new name to the node/edge  - with syntax nameof([subgraph]) = expression you can assign a new name to the host graph or the given subgraph  - added fronted and semantic tests

---
+++ frontend/de/unika/ipd/grgen/ir/exprevals/AssignmentNameof.java
@@ -0,0 +1,43 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 4.3
+ * Copyright (C) 2003-2014 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+/**
+ * @author Edgar Jakumeit
+ */
+
+package de.unika.ipd.grgen.ir.exprevals;
+
+
+/**
+ * Represents a nameof assignment statement in the IR.
+ */
+public class AssignmentNameof extends AssignmentBase {
+
+	/** The lhs of the assignment. */
+	private Expression target;
+
+	public AssignmentNameof(Expression target, Expression expr) {
+		super("assignment nameof");
+		this.target = target;
+		this.expr = expr;
+	}
+
+	public Expression getTarget() {
+		return target;
+	}
+
+	public String toString() {
+		return "nameof(" + (getTarget()!=null ? getTarget().toString() : "") + ") = " + getExpression();
+	}
+
+	public void collectNeededEntities(NeededEntities needs)
+	{
+		if(target!=null)
+			target.collectNeededEntities(needs);		
+		getExpression().collectNeededEntities(needs);
+	}
+}
