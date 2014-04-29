ed3225889ac61f29f6052972695a355e6fea5d28
eja <eja@localhost>
2014-04-29 11:57:23 +0200

    added ability to execute a sequence after applying the main rule to the movie database benchmarker  (useful for checking results)

--- engine-net-2/tools/MovieDatabaseBenchmarker/MovieDatabaseBenchmarker.cs
+++ engine-net-2/tools/MovieDatabaseBenchmarker/MovieDatabaseBenchmarker.cs
@@ -19,10 +19,11 @@ namespace MovieDatabase
     {
         static void Main(string[] args)
         {
-            if(args.Length != 2)
+            if(args.Length < 2 || args.Length > 3)
             {
-                Console.WriteLine("usage: MovieDatabaseBenchmarker <name of rule to apply> <name of grs file to import or number of creation iterations of synthetic graph>");
+                Console.WriteLine("usage: MovieDatabaseBenchmarker <name of rule to apply> <name of grs file to import or number of creation iterations of synthetic graph> [\"sequence to execute\"]");
                 Console.WriteLine("example: MovieDatabaseBenchmarker findCouplesOpt imdb-0005000-50176.movies.xmi.grs");
+                Console.WriteLine("example: MovieDatabaseBenchmarker findCliquesOf3Opt imdb-0130000-712130.movies.xmi.grs \"[cliques3WithRating\\orderDescendingBy<avgRating>\\keepFirst(15)] ;> [cliques3WithRating\\orderDescendingBy<numMovies>\\keepFirst(15)]\"");
                 return;
             }
 
@@ -73,9 +74,10 @@ namespace MovieDatabase
             graph.AnalyzeGraph();
             actions.GenerateActions(args[0]);
 
-            Console.WriteLine("Number of Movies: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Movie").TypeID]);
-            Console.WriteLine("Number of Persons: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Person").TypeID]);
-            Console.WriteLine("Number of personToMovies: " + graph.edgesByTypeCounts[graph.Model.EdgeModel.GetType("personToMovie").TypeID]);
+            Console.WriteLine("Number of Movie: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Movie").TypeID]);
+            Console.WriteLine("Number of Actor: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Actor").TypeID]);
+            Console.WriteLine("Number of Actress: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Actress").TypeID]);
+            Console.WriteLine("Number of personToMovie: " + graph.edgesByTypeCounts[graph.Model.EdgeModel.GetType("personToMovie").TypeID]);
 
             Console.WriteLine("Start matching " + args[0] + " ...");
 
@@ -93,8 +95,14 @@ namespace MovieDatabase
 
             Console.WriteLine("...needed " + (Environment.TickCount - startTime) + "ms for finding the matches and adding the couples/cliques");
 
-            Console.WriteLine("Number of Groups: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Group").TypeID]);
+            Console.WriteLine("Number of Couple: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Couple").TypeID]);
+            Console.WriteLine("Number of Clique: " + graph.nodesByTypeCounts[graph.Model.NodeModel.GetType("Clique").TypeID]);
             Console.WriteLine("Number of commonMovies: " + graph.edgesByTypeCounts[graph.Model.EdgeModel.GetType("commonMovies").TypeID]);
+
+            if(args.Length == 3)
+            {
+                procEnv.ApplyGraphRewriteSequence(args[2]);
+            }
         }
     }
 }
