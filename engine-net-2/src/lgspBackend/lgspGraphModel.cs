8d718cefb06df8b67956e12faf42e251cbe3e73b
eja <eja@localhost>
2014-04-29 11:26:33 +0200

    bugfix for index set creation/binding/filling   a LGSPGraphModel is not always available, reduced requirements to an IGraphModel

--- engine-net-2/src/lgspBackend/lgspGraphModel.cs
+++ engine-net-2/src/lgspBackend/lgspGraphModel.cs
@@ -30,20 +30,8 @@ namespace de.unika.ipd.grGen.lgsp
         public abstract IEnumerable<IndexDescription> IndexDescriptions { get; }
         public abstract bool GraphElementUniquenessIsEnsured { get; }
         public abstract bool GraphElementsAreAccessibleByUniqueId { get; }
-
-        /// <summary>
-        /// Called by the graph (generic implementation) to create and bind its index set (generated code).
-        /// Always called by an empty graph just constructed.
-        /// </summary>
-        public abstract void CreateAndBindIndexSet(LGSPGraph graph);
-
-        /// <summary>
-        /// Called on an index set that was created and bound,
-        /// when the graph was copy constructed from an original graph,
-        /// to fill in the already available cloned content from the original graph.
-        /// </summary>
-        public abstract void FillIndexSetAsClone(LGSPGraph graph, LGSPGraph originalGraph, IDictionary<IGraphElement, IGraphElement> oldToNewMap);
-
+        public abstract void CreateAndBindIndexSet(IGraph graph);
+        public abstract void FillIndexSetAsClone(IGraph graph, IGraph originalGraph, IDictionary<IGraphElement, IGraphElement> oldToNewMap);
 
         #region Emitting and parsing of attributes of object or a user defined type
 
