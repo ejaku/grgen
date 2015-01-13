8a55df112c4369034c1beec1093e38a56204f094
eja <eja@localhost>
2015-01-13 23:28:02 +0100

    yes, happy new year to you, too!

--- frontend/de/unika/ipd/grgen/ast/exprevals/FunctionMethodInvocationBaseNode.java
+++ frontend/de/unika/ipd/grgen/ast/exprevals/FunctionMethodInvocationBaseNode.java
@@ -1,3 +1,14 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 4.4
+ * Copyright (C) 2003-2015 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+/**
+ * @author Edgar Jakumeit
+ */
+
 package de.unika.ipd.grgen.ast.exprevals;
 
 import de.unika.ipd.grgen.ast.CollectNode;
