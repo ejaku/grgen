72f84ce18a845f1f29ab8fc5f61efb1d8f976546
eja <eja@localhost>
2015-10-06 21:13:06 +0200

    added bitbucket issues 2-7 from Peter Gr?ner as frontend tests

---
+++ frontend/test/should_pass/bitbucket2ModelExternalFunctionsImpl.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using GRGEN_LIBGR = de.unika.ipd.grGen.libGr;
+using GRGEN_LGSP = de.unika.ipd.grGen.lgsp;
+using GRGEN_MODEL = de.unika.ipd.grGen.Model_bitbucket2;
+
+namespace de.unika.ipd.grGen.expression
+{
+	public partial class ExternalFunctions
+	{
+		public static int TestFunction(GRGEN_LIBGR.IActionExecutionEnvironment actionEnv, GRGEN_LIBGR.IGraph graph)
+		{
+			return 42;
+		}
+	}
+}
+
+namespace de.unika.ipd.grGen.expression
+{
+	public partial class ExternalProcedures
+	{
+		public static void TestProcedure(GRGEN_LIBGR.IActionExecutionEnvironment actionEnv, GRGEN_LIBGR.IGraph graph)
+		{
+		}
+	}
+}
