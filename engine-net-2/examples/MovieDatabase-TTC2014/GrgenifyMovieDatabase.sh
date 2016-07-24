1bcc140b3e9f7d80dfb6d8b54ad1e171340bcf31
eja <eja@localhost>
2016-07-25 00:10:42 +0200

    applied an improved version of the fix for bitbucket issue #20 from Carl Mai

--- engine-net-2/examples/MovieDatabase-TTC2014/GrgenifyMovieDatabase.sh
+++ engine-net-2/examples/MovieDatabase-TTC2014/GrgenifyMovieDatabase.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+ï»¿#!/bin/bash
 
+#version piping in input from here document
+#for file in *.xmi; do	
+#    GrShell -N << HERE
+#import movies.ecore $file GrgenifyMovieDatabase.grg
+#redirect emit $file.grs
+#exec create_MovieDatabaseModel ;> [create_Movie] ;> [create_Actor] ;> [create_Actress] ;> [create_personToMovie]
+#redirect emit - 
+#validate exitonfailure exec noEdgesLeft
+#quit
+#HERE
+#done
+
+#version with -C command line option and non-newline-terminated grshell-commands
 for file in *.xmi; do	
-    GrShell -N << HERE
-import movies.ecore $file GrgenifyMovieDatabase.grg
-redirect emit $file.grs
-exec create_MovieDatabaseModel ;> [create_Movie] ;> [create_Actor] ;> [create_Actress] ;> [create_personToMovie]
-redirect emit - 
-validate exitonfailure exec noEdgesLeft
-quit
-HERE
+    GrShell -N -C "import movies.ecore $file GrgenifyMovieDatabase.grg ;; redirect emit $file.grs ;; exec create_MovieDatabaseModel ;> [create_Movie] ;> [create_Actor] ;> [create_Actress] ;> [create_personToMovie] #Â§ ;; redirect emit - ;; validate exitonfailure exec noEdgesLeft #Â§ ;; quit"
 done
 
-#above a here document is used to pipe some file content via stdin to GrShell; the script below would be a bit nicer, but I don't get it running
-#using cygwin it looks as if the input string is split at the blanks --> the -C "shell command lines" option is primarily of interest for windows cmd
+#version with -C command line option and verbatim grshell input / newline-terminated grshell-commands
 #for file in *.xmi; do	
-#    GrShell -N -C "import movies.ecore $file GrgenifyMovieDatabase.grg ;; redirect emit $file.grs ;; exec create_MovieDatabaseModel ;> [create_Movie] ;> [create_Actor] ;> [create_Actress] ;> [create_personToMovie] #§ ;; redirect emit - ;; validate exitonfailure exec noEdgesLeft #§ ;; quit"
+#    GrShell -N -C "import movies.ecore $file GrgenifyMovieDatabase.grg
+#redirect emit $file.grs
+#exec create_MovieDatabaseModel ;> [create_Movie] ;> [create_Actor] ;> [create_Actress] ;> [create_personToMovie]
+#redirect emit -
+#validate exitonfailure exec noEdgesLeft
+#quit"
 #done
