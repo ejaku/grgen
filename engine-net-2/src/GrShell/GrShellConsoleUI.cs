5340acd1746cab738e3b99f7c4d805fcfcf8de40
eja <eja@localhost>
2019-12-13 22:40:58 +0100

    split ShellGraphProcessingEnvironment.cs and GrShellConsoleUI.cs out of GrShellImpl.cs

---
+++ engine-net-2/src/GrShell/GrShellConsoleUI.cs
@@ -0,0 +1,77 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 4.5
+ * Copyright (C) 2003-2019 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+// by Moritz Kroll, Edgar Jakumeit
+
+using System;
+using System.IO;
+
+namespace de.unika.ipd.grGen.grShell
+{
+    public interface IGrShellUI
+    {
+        void ShowMsgAskForEnter(string msg);
+        bool ShowMsgAskForYesNo(string msg);
+        string ShowMsgAskForString(string msg);
+    }
+
+    public class EOFException : IOException
+    {
+    }
+
+    /// <summary>
+    /// Console interface for simple user prompts (e.g. for debugging)
+    /// </summary>
+    public class GrShellConsoleUI : IGrShellUI
+    {
+        protected TextReader in_;
+        protected TextWriter out_;
+
+        public GrShellConsoleUI(TextReader in_, TextWriter out_)
+        {
+            this.in_ = in_;
+            this.out_ = out_;
+        }
+
+        public string ReadOrEofErr()
+        {
+            string result = this.in_.ReadLine();
+            if (result == null) {
+                throw new EOFException();
+            }
+            return result;
+        }
+
+        public void ShowMsgAskForEnter(string msg)
+        {
+            this.out_.Write(msg + " [enter] ");
+            ReadOrEofErr();
+        }
+
+        public bool ShowMsgAskForYesNo(string msg)
+        {
+            while (true)
+            {
+                this.out_.Write(msg + " [y(es)/n(o)] ");
+                string result = ReadOrEofErr();
+                if (result.Equals("y", StringComparison.InvariantCultureIgnoreCase) ||
+                    result.Equals("yes", StringComparison.InvariantCultureIgnoreCase)) {
+                    return true;
+                } else if (result.Equals("n", StringComparison.InvariantCultureIgnoreCase) ||
+                    result.Equals("no", StringComparison.InvariantCultureIgnoreCase)) {
+                    return false;
+                }
+            }
+        }
+
+        public string ShowMsgAskForString(string msg)
+        {
+            this.out_.Write(msg);
+            return ReadOrEofErr();
+        }
+    }
+}
---
+++ engine-net-2/src/GrShell/ShellGraphProcessingEnvironment.cs
@@ -0,0 +1,74 @@
+/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 4.5
+ * Copyright (C) 2003-2019 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+// by Moritz Kroll, Edgar Jakumeit
+
+using System;
+using System.Collections.Generic;
+using ASTdapter;
+using de.unika.ipd.grGen.libGr;
+using de.unika.ipd.grGen.lgsp;
+
+namespace de.unika.ipd.grGen.grShell
+{
+    public class ShellGraphProcessingEnvironment
+    {
+        public IGraphProcessingEnvironment ProcEnv;
+
+        public DumpInfo DumpInfo;
+        public SubruleDebuggingConfiguration SubruleDebugConfig;
+        public VCGFlags VcgFlags = VCGFlags.OrientTopToBottom | VCGFlags.EdgeLabels;
+        public ParserPackage Parser = null;
+
+        public String BackendFilename;
+        public String[] BackendParameters;
+        public String ModelFilename;
+        public String ActionsFilename = null;
+
+        public Dictionary<string, INamedGraph> NameToSubgraph = new Dictionary<string, INamedGraph>(); // maps subgraph name to subgraph
+
+
+        public ShellGraphProcessingEnvironment(IGraph graph, String backendFilename, String[] backendParameters, String modelFilename)
+        {
+            LGSPNamedGraph Graph = new LGSPNamedGraph((LGSPGraph)graph);
+            DumpInfo = new DumpInfo(Graph.GetElementName);
+            SubruleDebugConfig = new SubruleDebuggingConfiguration();
+            BackendFilename = backendFilename;
+            BackendParameters = backendParameters;
+            ModelFilename = modelFilename;
+            ProcEnv = new LGSPGraphProcessingEnvironment(Graph, null);
+            NameToSubgraph.Add(Graph.Name, Graph);
+        }
+
+        public ShellGraphProcessingEnvironment(INamedGraph graph, String backendFilename, String[] backendParameters, String modelFilename)
+        {
+            LGSPNamedGraph Graph = (LGSPNamedGraph)graph;
+            DumpInfo = new DumpInfo(Graph.GetElementName);
+            SubruleDebugConfig = new SubruleDebuggingConfiguration();
+            BackendFilename = backendFilename;
+            BackendParameters = backendParameters;
+            ModelFilename = modelFilename;
+            ProcEnv = new LGSPGraphProcessingEnvironment(Graph, null);
+            NameToSubgraph.Add(Graph.Name, Graph);
+        }
+
+        public ShellGraphProcessingEnvironment Clone(string name)
+        {
+            string realname = (name == null) ? ProcEnv.Graph.Name + "-clone" : name;
+            ShellGraphProcessingEnvironment result = new ShellGraphProcessingEnvironment(ProcEnv.Graph.Clone(realname),
+                BackendFilename, BackendParameters, ModelFilename);
+            result.ProcEnv.Actions = this.ProcEnv.Actions;
+            result.DumpInfo = this.DumpInfo;
+            result.SubruleDebugConfig = this.SubruleDebugConfig;
+            result.VcgFlags = this.VcgFlags;
+            result.Parser = this.Parser;
+            result.ActionsFilename = this.ActionsFilename;
+            result.ProcEnv.EmitWriter = this.ProcEnv.EmitWriter;
+            return result;
+        }
+    }
+}
