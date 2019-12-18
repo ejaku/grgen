86373b4d11a4071ef65a44b1a3567dbbd3c5d3d6
eja <eja@localhost>
2019-12-18 19:51:25 +0100

    merged info classes of functions, procedures, and sequences into the files containing the corresponding interfaces to their definitions

--- engine-net-2/src/libGr/Actions/ISequenceDefinitionAndInfo.cs
+++ engine-net-2/src/libGr/Actions/ISequenceDefinitionAndInfo.cs
@@ -5,14 +5,45 @@
  * www.grgen.net
  */
 
-// by Edgar Jakumeit
+// by Moritz Kroll, Edgar Jakumeit
+
+//#define USE_HIGHPERFORMANCE_COUNTER
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace de.unika.ipd.grGen.libGr
 {
+    /// <summary>
+    /// An object representing an executable sequence.
+    /// </summary>
+    public interface ISequenceDefinition
+    {
+        /// <summary>
+        /// The name of the sequence
+        /// </summary>
+        String Name { get; }
+
+        /// <summary>
+        /// The annotations of the sequence
+        /// </summary>
+        Annotations Annotations { get; }
+
+        /// <summary>
+        /// Applies this sequence.
+        /// </summary>
+        /// <param name="sequenceInvocation">Sequence invocation object for this sequence application,
+        ///     containing the input parameter sources and output parameter targets</param>
+        /// <param name="procEnv">The graph processing environment on which this sequence is to be applied.
+        ///     Contains especially the graph on which this sequence is to be applied.
+        ///     The rules will only be chosen during the Sequence object instantiation, so
+        ///     exchanging rules will have no effect for already existing Sequence objects.</param>
+        /// <param name="env">The execution environment giving access to the names and user interface (null if not available)</param>
+        /// <returns>True, iff the sequence succeeded</returns>
+        bool Apply(SequenceInvocationParameterBindings sequenceInvocation,
+            IGraphProcessingEnvironment procEnv);
+    }
+
     /// <summary>
     /// Represents an XGRS used in an exec statement.
     /// </summary>
@@ -29,7 +60,7 @@ namespace de.unika.ipd.grGen.libGr
         /// <param name="xgrs">The XGRS string.</param>
         /// <param name="lineNr">The line number the sequence appears on in the source.</param>
         public EmbeddedSequenceInfo(String[] parameters, GrGenType[] parameterTypes,
-            String[] outParameters, GrGenType[] outParameterTypes, 
+            String[] outParameters, GrGenType[] outParameterTypes,
             String package, String xgrs, int lineNr)
         {
             Parameters = parameters;
