ejaku/grgen

commit d9f865ec173adde94bf72af68e7581fa88ae01f6

switched members to readonly where possible (introduced base class constructors where needed, adapted generator for derived classes to call the base class constructor, handing in the member values)

engine-net-2/src/lgspBackend/Actions/lgspActions.cs
@@ -25,6 +25,12 @@ namespace de.unika.ipd.grGen.lgsp
     /// </summary>
     public abstract class LGSPAction
     {
+        protected LGSPAction(PatternGraph patternGraph, object[] returnArray)
+        {
+            this.patternGraph = patternGraph;
+            this.ReturnArray = returnArray;
+        }
+
         /// <summary>
         /// The LGSPRulePattern object from which this LGSPAction object has been created.
         /// </summary>
@@ -33,13 +39,13 @@ namespace de.unika.ipd.grGen.lgsp
         /// <summary>
         /// The PatternGraph object of the main graph
         /// </summary>
-        public PatternGraph patternGraph;
+        public readonly PatternGraph patternGraph;
 
         /// <summary>
         /// Performance optimization: saves us usage of new for the return array or the return arrays. 
         /// In the object/string-style modify/apply methods of the action interface implementation.
         /// </summary>
-        public object[] ReturnArray;
+        public readonly object[] ReturnArray;
         public List<object[]> ReturnArrayListForAll = new List<object[]>();
         public List<object[]> AvailableReturnArrays = new List<object[]>();
 
@@ -68,15 +74,16 @@ namespace de.unika.ipd.grGen.lgsp
     public abstract class LGSPActions : BaseActions
     {
         private LGSPGraph graph;
-        private LGSPMatcherGenerator matcherGenerator;
-        private String modelAssemblyName;
-        private String actionsAssemblyName;
 
-        protected Dictionary<String, LGSPAction> actions = new Dictionary<String, LGSPAction>(); // action names -> action objects, filled by derived classes
+        private readonly LGSPMatcherGenerator matcherGenerator;
+        private readonly String modelAssemblyName;
+        private readonly String actionsAssemblyName;
+
+        protected readonly Dictionary<String, LGSPAction> actions = new Dictionary<String, LGSPAction>(); // action names -> action objects, filled by derived classes
 
         private static int actionID = 0;
 
-        protected Dictionary<String, String> customCommandsToDescriptions;
+        protected readonly Dictionary<String, String> customCommandsToDescriptions;
 
 
         /// <summary>
@@ -91,6 +98,7 @@ namespace de.unika.ipd.grGen.lgsp
             modelAssemblyName = Assembly.GetAssembly(graph.Model.GetType()).Location;
             actionsAssemblyName = Assembly.GetAssembly(this.GetType()).Location;
 
+            customCommandsToDescriptions = new Dictionary<string, string>();
             FillCustomCommandDescriptions();
 
 #if ASSERT_ALL_UNMAPPED_AFTER_MATCH
@@ -325,7 +333,6 @@ namespace de.unika.ipd.grGen.lgsp
 
         private void FillCustomCommandDescriptions()
         {
-            customCommandsToDescriptions = new Dictionary<string, string>();
             customCommandsToDescriptions.Add("gen_searchplan",
                 "- gen_searchplan: Generates a new searchplan for a given action\n" +
                 "     depending on a previous graph analysis.\n");
@@ -495,6 +502,11 @@ namespace de.unika.ipd.grGen.lgsp
     /// </summary>
     public abstract class LGSPSubpatternAction
     {
+        protected LGSPSubpatternAction(PatternGraph[] patternGraphs)
+        {
+            this.patternGraphs = patternGraphs;
+        }
+
         /// <summary>
         /// The PatternGraph object from which this matching task object has been created
         /// </summary>
@@ -504,7 +516,7 @@ namespace de.unika.ipd.grGen.lgsp
         /// The PatternGraph objects from which this matching task object has been created
         /// (non-null in case of an alternative, contains the pattern graphs of the alternative cases then)
         /// </summary>
-        protected PatternGraph[] patternGraphs;
+        protected readonly PatternGraph[] patternGraphs;
 
         /// <summary>
         /// The action execution environment which contains the host graph in which to search for matches

engine-net-2/src/lgspBackend/MatcherGenerator/lgspMatcherGenerator.cs
@@ -1344,10 +1344,11 @@ namespace de.unika.ipd.grGen.lgsp
             sb.AppendFront("{\n");
             sb.Indent(); // class level
 
-            sb.AppendFront("public " + className + "() {\n");
+            sb.AppendFront("public " + className + "()\n");
+            sb.AppendFrontFormat("    : base(" + rulePatternClassName + ".Instance.patternGraph, new object[{0}])\n", rulePattern.Outputs.Length);
+            sb.AppendFront("{\n");
             sb.Indent(); // method body level
             sb.AppendFront("_rulePattern = " + rulePatternClassName + ".Instance;\n");
-            sb.AppendFront("patternGraph = _rulePattern.patternGraph;\n");
             if(rulePattern.patternGraph.branchingFactor < 2)
             {
                 sb.AppendFront("DynamicMatch = myMatch;\n");
@@ -1389,7 +1390,6 @@ namespace de.unika.ipd.grGen.lgsp
                 sb.Unindent();
                 sb.AppendFront("}\n");
             }
-            sb.AppendFrontFormat("ReturnArray = new object[{0}];\n", rulePattern.Outputs.Length);
             sb.AppendFront("matches = new GRGEN_LGSP.LGSPMatchesList<" + matchClassName +", " + matchInterfaceName + ">(this);\n");
             sb.Unindent(); // class level
             sb.AppendFront("}\n\n");
@@ -1547,7 +1547,9 @@ namespace de.unika.ipd.grGen.lgsp
             sb.AppendFront("{\n");
             sb.Indent(); // class level
 
-            sb.AppendFront("private " + className + "(GRGEN_LGSP.LGSPActionExecutionEnvironment actionEnv_, Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_) {\n");
+            sb.AppendFront("private " + className + "(GRGEN_LGSP.LGSPActionExecutionEnvironment actionEnv_, Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_)\n");
+            sb.AppendFrontFormat("    : base(null)\n");
+            sb.AppendFront("{\n");
             sb.Indent(); // method body level
             sb.AppendFront("actionEnv = actionEnv_; openTasks = openTasks_;\n");
             sb.AppendFront("patternGraph = " + matchingPatternClassName + ".Instance.patternGraph;\n");
@@ -1607,12 +1609,13 @@ namespace de.unika.ipd.grGen.lgsp
             sb.AppendFront("{\n");
             sb.Indent(); // class level
 
+
             sb.AppendFront("private " + className + "(GRGEN_LGSP.LGSPActionExecutionEnvironment actionEnv_, "
-                + "Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_, GRGEN_LGSP.PatternGraph[] patternGraphs_) {\n");
+                + "Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_, GRGEN_LGSP.PatternGraph[] patternGraphs_)\n");
+            sb.AppendFrontFormat("    : base(patternGraphs_)\n");
+            sb.AppendFront("{\n");
             sb.Indent(); // method body level
             sb.AppendFront("actionEnv = actionEnv_; openTasks = openTasks_;\n");
-            // pfadausdruck gebraucht, da das alternative-objekt im pattern graph steckt
-            sb.AppendFront("patternGraphs = patternGraphs_;\n");
             
             sb.Unindent(); // class level
             sb.AppendFront("}\n\n");
@@ -1676,7 +1679,9 @@ namespace de.unika.ipd.grGen.lgsp
             sb.AppendFront("{\n");
             sb.Indent(); // class level
 
-            sb.AppendFront("private " + className + "(GRGEN_LGSP.LGSPActionExecutionEnvironment actionEnv_, Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_) {\n");
+            sb.AppendFront("private " + className + "(GRGEN_LGSP.LGSPActionExecutionEnvironment actionEnv_, Stack<GRGEN_LGSP.LGSPSubpatternAction> openTasks_)\n");
+            sb.AppendFrontFormat("    : base(null)\n");
+            sb.AppendFront("{\n");
             sb.Indent(); // method body level
             sb.AppendFront("actionEnv = actionEnv_; openTasks = openTasks_;\n");
             sb.AppendFront("patternGraph = " + matchingPatternClassName + ".Instance.patternGraph;\n");
@@ -1781,9 +1786,7 @@ namespace de.unika.ipd.grGen.lgsp
             sb.Indent();
             sb.AppendFront("newTask = freeListHead;\n"); 
             sb.AppendFront("newTask.actionEnv = actionEnv_; newTask.openTasks = openTasks_;\n");
-            if(isAlternative)
-                sb.AppendFront("newTask.patternGraphs = patternGraphs_;\n");
-            else if(isIterationBreaking)
+            if(isIterationBreaking)
                 sb.AppendFront("newTask.breakIteration = false;\n");
             sb.AppendFront("freeListHead = newTask.next;\n");
             sb.AppendFront("newTask.next = null;\n");

commit bb4fac96823034b59f601f1e8d68342013fbe6df

fixes for bugs introduced in commit d9f865ec from 19.01.20 (creation of customCommandsToDescriptions was moved only partially, missing in one constructor; missing removal of patternGraphs assignment in alternative task creation, now carried out only in constructor)

engine-net-2/src/lgspBackend/Actions/lgspActions.cs
@@ -120,6 +120,7 @@ namespace de.unika.ipd.grGen.lgsp
             actionsAssemblyName = actionsAsmName;
             matcherGenerator = new LGSPMatcherGenerator(graph.Model);
 
+            customCommandsToDescriptions = new Dictionary<string, string>();
             FillCustomCommandDescriptions();
 
 #if ASSERT_ALL_UNMAPPED_AFTER_MATCH

engine-net-2/src/lgspBackend/MatcherGenerator/lgspMatcherGenerator.cs
@@ -1835,9 +1835,7 @@ namespace de.unika.ipd.grGen.lgsp
                 sb.Indent();
                 sb.AppendFront("newTask = freeListHead_perWorker[threadId];\n");
                 sb.AppendFront("newTask.actionEnv = actionEnv_; newTask.openTasks = openTasks_;\n");
-                if(isAlternative)
-                    sb.AppendFront("newTask.patternGraphs = patternGraphs_;\n");
-                else if(isIterationBreaking)
+                if(isIterationBreaking)
                     sb.AppendFront("newTask.breakIteration = false;\n");
                 sb.AppendFront("freeListHead_perWorker[threadId] = newTask.next;\n");
                 sb.AppendFront("newTask.next = null;\n");