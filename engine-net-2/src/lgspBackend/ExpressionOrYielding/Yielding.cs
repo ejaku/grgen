ejaku/grgen

commit 6d8a35f075f2bb8284731aad9f00d727fc03922c

decoupled creation of uniqueness handler and index a bit from the graph - tightened protection level for indices and uniquenessEnsurer in the graph - they are now assigned in the graph and only created in the model (instead of getting injected upon creation from the outside into the graph) - added an IUniquenessHandler to the model interface (without functionality, but representing the LGSPUniquenessEnsurer and LGSPUniquenessIndex classes from the model implementation) - adapted model generator (to only create and return, and FillAsClone only on the indices instead of on the graph)

frontend/de/unika/ipd/grgen/be/Csharp/ModelGen.java
@@ -4499,18 +4499,23 @@ commonLoop:	for(InheritanceType commonType : firstCommonAncestors) {
 	}
 
 	private void genIndicesGraphBinding() {
-		sb.append("\t\tpublic override void CreateAndBindIndexSet(GRGEN_LIBGR.IGraph graph) {\n");
+		sb.append("\t\tpublic override GRGEN_LIBGR.IUniquenessHandler CreateUniquenessHandler(GRGEN_LIBGR.IGraph graph) {\n");
 		if(model.isUniqueIndexDefined())
-			sb.append("\t\t\tnew GRGEN_LGSP.LGSPUniquenessIndex((GRGEN_LGSP.LGSPGraph)graph); // must be called before the indices so that its event handler is registered first, doing the unique id computation the indices depend upon\n");
+			sb.append("\t\t\treturn new GRGEN_LGSP.LGSPUniquenessIndex((GRGEN_LGSP.LGSPGraph)graph); // must be called before the indices so that its event handler is registered first, doing the unique id computation the indices depend upon\n");
 		else if(model.isUniqueDefined())
-			sb.append("\t\t\tnew GRGEN_LGSP.LGSPUniquenessEnsurer((GRGEN_LGSP.LGSPGraph)graph); // must be called before the indices so that its event handler is registered first, doing the unique id computation the indices depend upon\n");
-		sb.append("\t\t\t((GRGEN_LGSP.LGSPGraph)graph).indices = new " + model.getIdent() + "IndexSet((GRGEN_LGSP.LGSPGraph)graph);\n");
+			sb.append("\t\t\treturn new GRGEN_LGSP.LGSPUniquenessEnsurer((GRGEN_LGSP.LGSPGraph)graph); // must be called before the indices so that its event handler is registered first, doing the unique id computation the indices depend upon\n");
+		else
+			sb.append("\t\t\treturn null;\n");
+		sb.append("\t\t}\n");
+
+		sb.append("\t\tpublic override GRGEN_LIBGR.IIndexSet CreateIndexSet(GRGEN_LIBGR.IGraph graph) {\n");
+		sb.append("\t\t\treturn new " + model.getIdent() + "IndexSet((GRGEN_LGSP.LGSPGraph)graph);\n");
 		sb.append("\t\t}\n");
 		
 		sb.append("\t\tpublic override void FillIndexSetAsClone(GRGEN_LIBGR.IGraph graph, GRGEN_LIBGR.IGraph originalGraph, IDictionary<GRGEN_LIBGR.IGraphElement, GRGEN_LIBGR.IGraphElement> oldToNewMap) {\n");
 		if(model.isUniqueDefined())
-			sb.append("\t\t\t((GRGEN_LGSP.LGSPGraph)graph).uniquenessEnsurer.FillAsClone((GRGEN_LGSP.LGSPGraph)originalGraph, oldToNewMap);\n");
-		sb.append("\t\t\t((" + model.getIdent() + "IndexSet)((GRGEN_LGSP.LGSPGraph)graph).indices).FillAsClone((GRGEN_LGSP.LGSPGraph)originalGraph, oldToNewMap);\n");
+			sb.append("\t\t\t((GRGEN_LGSP.LGSPUniquenessEnsurer)graph.UniquenessHandler).FillAsClone((GRGEN_LGSP.LGSPUniquenessEnsurer)originalGraph.UniquenessHandler, oldToNewMap);\n");
+		sb.append("\t\t\t((" + model.getIdent() + "IndexSet)graph.Indices).FillAsClone((GRGEN_LGSP.LGSPGraph)originalGraph, oldToNewMap);\n");
 		sb.append("\t\t}\n");
 	}
 	

engine-net-2/src/lgspBackend/Graph/lgspGraph.cs
@@ -38,8 +38,8 @@ namespace de.unika.ipd.grGen.lgsp
         protected readonly IGraphModel model;
         internal readonly String modelAssemblyName;
 
-        public IIndexSet indices;
-        public LGSPUniquenessEnsurer uniquenessEnsurer; // not null if unique ids for nodes/edges were requested
+        private IIndexSet indices;
+        private LGSPUniquenessEnsurer uniquenessEnsurer; // not null if unique ids for nodes/edges were requested
 
         // Used as storage space for the name for the SettingAddedEdgeNames event, in case of redirection
         public string[] nameOfSingleElementAdded = new string[1];
@@ -294,7 +294,8 @@ namespace de.unika.ipd.grGen.lgsp
             }
             edgesByTypeCounts = new int[model.EdgeModel.Types.Length];
 
-            model.CreateAndBindIndexSet(this);
+            uniquenessEnsurer = (LGSPUniquenessEnsurer)model.CreateUniquenessHandler(this);
+            indices = model.CreateIndexSet(this);
 
             statistics = new LGSPGraphStatistics(model);
 
@@ -352,6 +353,11 @@ namespace de.unika.ipd.grGen.lgsp
         /// The indices associated with the graph.
         /// </summary>
         public override IIndexSet Indices { get { return indices; } }
+
+        /// <summary>
+        /// The uniqueness handler associated with the graph.
+        /// </summary>
+        public override IUniquenessHandler UniquenessHandler { get { return uniquenessEnsurer; } }
 
         /// <summary>
         /// Returns the number of nodes with the exact given node type.

engine-net-2/src/lgspBackend/Graph/lgspGraphModel.cs
@@ -31,7 +31,8 @@ namespace de.unika.ipd.grGen.lgsp
         public abstract bool GraphElementUniquenessIsEnsured { get; }
         public abstract bool GraphElementsAreAccessibleByUniqueId { get; }
         public abstract int BranchingFactorForEqualsAny { get; }
-        public abstract void CreateAndBindIndexSet(IGraph graph);
+        public abstract IUniquenessHandler CreateUniquenessHandler(IGraph graph);
+        public abstract IIndexSet CreateIndexSet(IGraph graph);
         public abstract void FillIndexSetAsClone(IGraph graph, IGraph originalGraph, IDictionary<IGraphElement, IGraphElement> oldToNewMap);
 
         #region Emitting and parsing of attributes of object or a user defined type

engine-net-2/src/lgspBackend/Graph/lgspUniquenessEnsurer.cs
@@ -20,7 +20,7 @@ namespace de.unika.ipd.grGen.lgsp
     /// A class ensuring unique ids for nodes and edges with a minimum amount of gaps.
     /// Gets instantiated in case support for unique nodes/edges was declared in the model.
     /// </summary>
-    public class LGSPUniquenessEnsurer
+    public class LGSPUniquenessEnsurer : IUniquenessHandler
     {
         public LGSPUniquenessEnsurer(LGSPGraph graph)
         {
@@ -31,7 +31,6 @@ namespace de.unika.ipd.grGen.lgsp
                 throw new Exception("Graph must be empty!");
 
             this.graph = graph;
-            graph.uniquenessEnsurer = this;
             
             // global counter for fetching a new unique id
             nextNewId = 0;
@@ -49,10 +48,8 @@ namespace de.unika.ipd.grGen.lgsp
             graph.OnRetypingEdge += RetypingEdge;
         }
 
-        public virtual void FillAsClone(LGSPGraph originalGraph, IDictionary<IGraphElement, IGraphElement> oldToNewMap)
+        public virtual void FillAsClone(LGSPUniquenessEnsurer original, IDictionary<IGraphElement, IGraphElement> oldToNewMap)
         {
-            LGSPUniquenessEnsurer original = originalGraph.uniquenessEnsurer;
-
             nextNewId = original.nextNewId;
 
             heap.Clear(); // remove the -1
@@ -270,18 +267,18 @@ namespace de.unika.ipd.grGen.lgsp
             index.Clear();
         }
 
-        public override void FillAsClone(LGSPGraph originalGraph, IDictionary<IGraphElement, IGraphElement> oldToNewMap)
+        public override void FillAsClone(LGSPUniquenessEnsurer original, IDictionary<IGraphElement, IGraphElement> oldToNewMap)
         {
-            base.FillAsClone(originalGraph, oldToNewMap);
+            base.FillAsClone(original, oldToNewMap);
  
-            LGSPUniquenessIndex original = (LGSPUniquenessIndex)originalGraph.uniquenessEnsurer;
+            LGSPUniquenessIndex originalIndex = (LGSPUniquenessIndex)original;
 
-            if(original.index != null)
+            if(originalIndex.index != null)
             {
-                index = new List<IGraphElement>(original.index.Capacity);
-                for(int i = 0; i < original.index.Count; ++i)
+                index = new List<IGraphElement>(originalIndex.index.Capacity);
+                for(int i = 0; i < originalIndex.index.Count; ++i)
                 {
-                    index.Add(oldToNewMap[original.index[i]]);
+                    index.Add(oldToNewMap[originalIndex.index[i]]);
                 }
             }
         }

engine-net-2/src/libGr/Graph/BaseGraph.cs
@@ -22,6 +22,7 @@ namespace de.unika.ipd.grGen.libGr
         public abstract int GraphId { get; }
         public abstract IGraphModel Model { get; }
         public abstract IIndexSet Indices { get; }
+        public abstract IUniquenessHandler UniquenessHandler { get; }
         public abstract bool ReuseOptimization { get; set; }
         public abstract long ChangesCounter{ get; }
 

engine-net-2/src/libGr/Graph/IGraph.cs
@@ -210,6 +210,11 @@ namespace de.unika.ipd.grGen.libGr
         IIndexSet Indices { get; }
 
         /// <summary>
+        /// The uniqueness handler associated with the graph.
+        /// </summary>
+        IUniquenessHandler UniquenessHandler { get; }
+
+        /// <summary>
         /// If true (the default case), elements deleted during a rewrite
         /// may be reused in the same rewrite.
         /// As a result new elements may not be discriminable anymore from

engine-net-2/src/libGr/Graph/IGraphModel.cs
@@ -70,10 +70,16 @@ namespace de.unika.ipd.grGen.libGr
         int BranchingFactorForEqualsAny { get; }
 
         /// <summary>
-        /// Called by the graph (generic implementation) to create and bind its index set (generated code).
-        /// Always called by an empty graph just constructed.
+        /// Called by the graph (generic implementation) to create its uniqueness handler (generated code).
+        /// Always called by an empty graph just constructed, the uniqueness handler is then directly bound to the graph.
         /// </summary>
-        void CreateAndBindIndexSet(IGraph graph);
+        IUniquenessHandler CreateUniquenessHandler(IGraph graph);
+
+        /// <summary>
+        /// Called by the graph (generic implementation) to create its index set (generated code).
+        /// Always called by an empty graph just constructed, the index set is then directly bound to the graph.
+        /// </summary>
+        IIndexSet CreateIndexSet(IGraph graph);
 
         /// <summary>
         /// Called on an index set that was created and bound,

engine-net-2/src/libGr/Graph/IIndexSet.cs
@@ -198,4 +198,11 @@ namespace de.unika.ipd.grGen.libGr
         /// </summary>
         IIndex GetIndex(string indexName);
     }
+
+    /// <summary>
+    /// Base class for uniqueness handlers, only needed as a kind of handle on libGr level, to transfer real implementation objects.
+    /// </summary>
+    public interface IUniquenessHandler
+    {
+    }
 }

commit 6a2173b6a2a9c5485a5f6a0e63ab63f1f5d3f76a

fix for bug introduced in commit 6d8a35f0 from 20.01.20 (missing adaptation to protection level tightening, indices must by accessed by public Indices getter now instead of private indices member variable)

engine-net-2/src/lgspBackend/ExpressionOrYielding/Yielding.cs
@@ -1526,7 +1526,7 @@ namespace de.unika.ipd.grGen.expression
 
         public override void Emit(SourceBuilder sourceCode)
         {
-            sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.indices).{3}.Lookup(", 
+            sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.Indices).{3}.Lookup(", 
                 VariableType, NamesOfEntities.Variable(Variable), IndexSetType, Index.Name);
             Expr.Emit(sourceCode);
             sourceCode.Append("))\n");
@@ -1609,7 +1609,7 @@ namespace de.unika.ipd.grGen.expression
 
         public override void Emit(SourceBuilder sourceCode)
         {
-            sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.indices).{3}.Lookup",
+            sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.Indices).{3}.Lookup",
                 VariableType, NamesOfEntities.Variable(Variable), IndexSetType, Index.Name);
 
             if(Ascending)

engine-net-2/src/lgspBackend/SearchProgramOperations/SearchProgramOperationGetCandidate.cs
@@ -404,7 +404,7 @@ namespace de.unika.ipd.grGen.lgsp
                 // emit loop header with variable containing container entry
                 string indexIterationVariable =
                     NamesOfEntities.CandidateIterationIndexEntry(PatternElementName);
-                sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.indices).{3}.",
+                sourceCode.AppendFrontFormat("foreach({0} {1} in (({2})graph.Indices).{3}.",
                         IterationType, indexIterationVariable, IndexSetType, IndexName);
                 if(IndexAccessType==IndexAccessType.Equality)
                 {
@@ -1622,7 +1622,7 @@ namespace de.unika.ipd.grGen.lgsp
                 // initialize variable containing candidates from parallelized next candidate
                 string variableContainingParallelizedIterator =
                     NamesOfEntities.IterationParallelizationIterator(PatternElementName);
-                sourceCode.AppendFrontFormat("{0} = (({1})graph.indices).{2}.",
+                sourceCode.AppendFrontFormat("{0} = (({1})graph.Indices).{2}.",
                     variableContainingParallelizedIterator, IndexSetType, IndexName);
 
                 if(IndexAccessType == IndexAccessType.Equality)
@@ -1649,7 +1649,7 @@ namespace de.unika.ipd.grGen.lgsp
                 }
 
                 // emit prerun determining the number of threads to wake up             
-                sourceCode.AppendFrontFormat("foreach({0} indexPreIteration in (({1})graph.indices).{2}.",
+                sourceCode.AppendFrontFormat("foreach({0} indexPreIteration in (({1})graph.Indices).{2}.",
                         IterationType, IndexSetType, IndexName);
                 if(IndexAccessType == IndexAccessType.Equality)
                 {

frontend/de/unika/ipd/grgen/be/Csharp/ModelGen.java
@@ -3292,7 +3292,7 @@ commonLoop:	for(InheritanceType commonType : firstCommonAncestors) {
 		sb.append("\t\t{\n");
 		for(Index index : model.getIndices()) {
 			String indexName = index.getIdent().toString();
-			sb.append("\t\t\t" + indexName + ".FillAsClone((Index" + indexName + "Impl)originalGraph.indices.GetIndex(\"" + indexName + "\"), oldToNewMap);\n");
+			sb.append("\t\t\t" + indexName + ".FillAsClone((Index" + indexName + "Impl)originalGraph.Indices.GetIndex(\"" + indexName + "\"), oldToNewMap);\n");
 		}
 		sb.append("\t\t}\n");
 		

frontend/de/unika/ipd/grgen/be/Csharp/ModifyGen.java
@@ -4027,7 +4027,7 @@ public class ModifyGen extends CSharpBase {
 
         sb.append("\t\t\tforeach( " + formatElementInterfaceRef(fiae.getIterationVar().getType()) +
         		" " + formatEntity(fiae.getIterationVar()) + " in ((" +
-        		"GRGEN_MODEL." + model.getIdent() + "IndexSet" + ")graph.indices)." + iae.index.getIdent() +
+        		"GRGEN_MODEL." + model.getIdent() + "IndexSet" + ")graph.Indices)." + iae.index.getIdent() +
         		".Lookup(");
 		genExpression(sb, iae.expr, state);	        
         sb.append(") )");
@@ -4050,7 +4050,7 @@ public class ModifyGen extends CSharpBase {
 
         sb.append("\t\t\tforeach( " + formatElementInterfaceRef(fiao.getIterationVar().getType()) +
         		" " + formatEntity(fiao.getIterationVar()) + " in ((" +
-        		"GRGEN_MODEL." + model.getIdent() + "IndexSet" + ")graph.indices)." + iao.index.getIdent() +
+        		"GRGEN_MODEL." + model.getIdent() + "IndexSet" + ")graph.Indices)." + iao.index.getIdent() +
         		".Lookup");
         if(iao.ascending)
         	sb.append("Ascending");