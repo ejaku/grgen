ejaku/grgen

commit c964ca56dadf3607b8f9c06da93abae4abd0f4b8

- removed code duplication: introduced/used helper methods for event firing - carried out code cosmetics: rename, condition merge

engine-net-2/src/libGr/Sequence/SequenceAssignmentTarget.cs
@@ -96,6 +96,19 @@ namespace de.unika.ipd.grGen.libGr
         }
 
         public override sealed int Precedence { get { return 9; } } // irrelevant, always top prio
+
+
+        #region helper methods
+
+        protected static void ChangingAttributeAssignElement(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, object firstValue, object optionalSecondValue)
+        {
+            if(elem is INode)
+                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, AttributeChangeType.Assign, firstValue, optionalSecondValue);
+            else
+                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, AttributeChangeType.Assign, firstValue, optionalSecondValue);
+        }
+
+        #endregion helper methods
     }
 
 
@@ -334,16 +347,12 @@ namespace de.unika.ipd.grGen.libGr
             AttributeType attrType;
             value = ContainerHelper.IfAttributeOfElementIsContainerThenCloneContainer(
                 elem, AttributeName, value, out attrType);
-            AttributeChangeType changeType = AttributeChangeType.Assign;
-            if(elem is INode)
-                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, changeType, value, null);
-            else
-                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, changeType, value, null);
+
+            ChangingAttributeAssignElement(procEnv, elem, attrType, value, null);
+
             elem.SetAttribute(AttributeName, value);
-            if(elem is INode)
-                procEnv.Graph.ChangedNodeAttribute((INode)elem, attrType);
-            else
-                procEnv.Graph.ChangedEdgeAttribute((IEdge)elem, attrType);
+
+            ChangedAttribute(procEnv, elem, attrType);
         }
 
         public override void GetLocalVariables(Dictionary<SequenceVariable, SetValueType> variables,
@@ -444,13 +453,9 @@ namespace de.unika.ipd.grGen.libGr
             IGraphElement elem = (IGraphElement)DestVar.GetVariableValue(procEnv);
             object container = elem.GetAttribute(AttributeName);
             object key = KeyExpression.Evaluate(procEnv);
-
             AttributeType attrType = elem.Type.GetAttributeType(AttributeName);
-            AttributeChangeType changeType = AttributeChangeType.AssignElement;
-            if(elem is INode)
-                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, changeType, value, key);
-            else
-                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, changeType, value, key);
+
+            ChangingAttributeAssignElement(procEnv, elem, attrType, value, key);
 
             if(container is IList)
             {
@@ -468,10 +473,7 @@ namespace de.unika.ipd.grGen.libGr
                 map[key] = value;
             }
 
-            if(elem is INode)
-                procEnv.Graph.ChangedNodeAttribute((INode)elem, attrType);
-            else
-                procEnv.Graph.ChangedEdgeAttribute((IEdge)elem, attrType);
+            ChangedAttribute(procEnv, elem, attrType);
         }
 
         public override void GetLocalVariables(Dictionary<SequenceVariable, SetValueType> variables,

engine-net-2/src/libGr/Sequence/SequenceBase.cs
@@ -375,6 +375,14 @@ namespace de.unika.ipd.grGen.libGr
             }
         }
 
+        protected static void ChangedAttribute(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType)
+        {
+            if(elem is INode)
+                procEnv.Graph.ChangedNodeAttribute((INode)elem, attrType);
+            else
+                procEnv.Graph.ChangedEdgeAttribute((IEdge)elem, attrType);
+        }
+
         #endregion helper methods
     }
 }

engine-net-2/src/libGr/Sequence/SequenceComputation.cs
@@ -297,14 +297,6 @@ namespace de.unika.ipd.grGen.libGr
                 procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, AttributeChangeType.RemoveElement, null, valueOrKeyOrIndexToRemove);
         }
 
-        protected static void ChangedAttribute(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType)
-        {
-            if(elem is INode)
-                procEnv.Graph.ChangedNodeAttribute((INode)elem, attrType);
-            else
-                procEnv.Graph.ChangedEdgeAttribute((IEdge)elem, attrType);
-        }
-
         #endregion helper methods
     }
 
@@ -658,30 +650,30 @@ namespace de.unika.ipd.grGen.libGr
             return deque;
         }
 
-        private object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap, object firstValue, object optionalSecondValue)
+        private object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary set, object firstValue, object optionalSecondValue)
         {
             if(elem != null)
                 ChangingSetAttributePutElement(procEnv, elem, attrType, firstValue);
 
-            setmap[firstValue] = optionalSecondValue;
+            set[firstValue] = optionalSecondValue;
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return set;
         }
 
-        private object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap, object firstValue, object optionalSecondValue)
+        private object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary map, object firstValue, object optionalSecondValue)
         {
             if(elem != null)
                 ChangingMapAttributePutElement(procEnv, elem, attrType, firstValue, optionalSecondValue);
 
-            setmap[firstValue] = optionalSecondValue;
+            map[firstValue] = optionalSecondValue;
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return map;
         }
 
         public override void GetLocalVariables(Dictionary<SequenceVariable, SetValueType> variables,
@@ -816,30 +808,30 @@ namespace de.unika.ipd.grGen.libGr
             return deque;
         }
 
-        private static object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap, object valueOrKeyOrIndexToRemove)
+        private static object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary set, object valueOrKeyOrIndexToRemove)
         {
             if(elem != null)
                 ChangingSetAttributeRemoveElement(procEnv, elem, attrType, valueOrKeyOrIndexToRemove);
 
-            setmap.Remove(valueOrKeyOrIndexToRemove);
+            set.Remove(valueOrKeyOrIndexToRemove);
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return set;
         }
 
-        private static object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap, object valueOrKeyOrIndexToRemove)
+        private static object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary map, object valueOrKeyOrIndexToRemove)
         {
             if(elem != null)
                 ChangingMapAttributeRemoveElement(procEnv, elem, attrType, valueOrKeyOrIndexToRemove);
 
-            setmap.Remove(valueOrKeyOrIndexToRemove);
+            map.Remove(valueOrKeyOrIndexToRemove);
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return map;
         }
 
         public override void GetLocalVariables(Dictionary<SequenceVariable, SetValueType> variables,
@@ -952,40 +944,40 @@ namespace de.unika.ipd.grGen.libGr
             return deque;
         }
 
-        private static object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap)
+        private static object ExecuteSet(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary set)
         {
             if(elem != null)
             {
-                foreach(DictionaryEntry kvp in setmap)
+                foreach(DictionaryEntry kvp in set)
                 {
                     ChangingSetAttributeRemoveElement(procEnv, elem, attrType, kvp.Key);
                 }
             }
 
-            setmap.Clear();
+            set.Clear();
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return set;
         }
 
-        private static object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary setmap)
+        private static object ExecuteMap(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, IDictionary map)
         {
             if(elem != null)
             {
-                foreach(DictionaryEntry kvp in setmap)
+                foreach(DictionaryEntry kvp in map)
                 {
                     ChangingMapAttributeRemoveElement(procEnv, elem, attrType, kvp.Key);
                 }
             }
 
-            setmap.Clear();
+            map.Clear();
 
             if(elem != null)
                 ChangedAttribute(procEnv, elem, attrType);
 
-            return setmap;
+            return map;
         }
 
         public override void GetLocalVariables(Dictionary<SequenceVariable, SetValueType> variables,
@@ -1783,21 +1775,15 @@ namespace de.unika.ipd.grGen.libGr
                 else
                     CheckNodeTypeIsKnown(env, Expr, ", first argument");
             }
-            if(ExprSrc != null)
+            if(ExprSrc != null && ExprSrc.Type(env) != "")
             {
-                if(ExprSrc.Type(env) != "")
-                {
-                    if(!TypesHelper.IsSameOrSubtype(ExprSrc.Type(env), "Node", env.Model))
-                        throw new SequenceParserException(Symbol + "second argument", "node", ExprSrc.Type(env));
-                }
+                if(!TypesHelper.IsSameOrSubtype(ExprSrc.Type(env), "Node", env.Model))
+                    throw new SequenceParserException(Symbol + "second argument", "node", ExprSrc.Type(env));
             }
-            if(ExprDst != null)
+            if(ExprDst != null && ExprDst.Type(env) != "")
             {
-                if(ExprDst.Type(env) != "")
-                {
-                    if(!TypesHelper.IsSameOrSubtype(ExprDst.Type(env), "Node", env.Model))
-                        throw new SequenceParserException(Symbol + "third argument", "node", ExprDst.Type(env));
-                }
+                if(!TypesHelper.IsSameOrSubtype(ExprDst.Type(env), "Node", env.Model))
+                    throw new SequenceParserException(Symbol + "third argument", "node", ExprDst.Type(env));
             }
         }
 
@@ -2094,21 +2080,15 @@ namespace de.unika.ipd.grGen.libGr
                         throw new SequenceParserException(Symbol + "first argument", "node", Expr.Type(env));
                 }
             }
-            if(ExprSrc != null)
+            if(ExprSrc != null && ExprSrc.Type(env) != "")
             {
-                if(ExprSrc.Type(env) != "")
-                {
-                    if(!TypesHelper.IsSameOrSubtype(ExprSrc.Type(env), "Node", env.Model))
-                        throw new SequenceParserException(Symbol + "second argument", "node", ExprSrc.Type(env));
-                }
+                if(!TypesHelper.IsSameOrSubtype(ExprSrc.Type(env), "Node", env.Model))
+                    throw new SequenceParserException(Symbol + "second argument", "node", ExprSrc.Type(env));
             }
-            if(ExprDst != null)
+            if(ExprDst != null && ExprDst.Type(env) != "")
             {
-                if(ExprDst.Type(env) != "")
-                {
-                    if(!TypesHelper.IsSameOrSubtype(ExprDst.Type(env), "Node", env.Model))
-                        throw new SequenceParserException(Symbol + "third argument", "node", ExprDst.Type(env));
-                }
+                if(!TypesHelper.IsSameOrSubtype(ExprDst.Type(env), "Node", env.Model))
+                    throw new SequenceParserException(Symbol + "third argument", "node", ExprDst.Type(env));
             }
         }
 

commit ce117a452042e5f13d3c3efbf8e5150a563c843f

fix for bug introduced in commit c964ca56 from 30.01.20 (incorrect helper function called/missing helper function)

engine-net-2/src/libGr/Sequence/SequenceAssignmentTarget.cs
@@ -100,12 +100,20 @@ namespace de.unika.ipd.grGen.libGr
 
         #region helper methods
 
-        protected static void ChangingAttributeAssignElement(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, object firstValue, object optionalSecondValue)
+        protected static void ChangingAttributeAssign(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, object value)
         {
             if(elem is INode)
-                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, AttributeChangeType.Assign, firstValue, optionalSecondValue);
+                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, AttributeChangeType.Assign, value, null);
             else
-                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, AttributeChangeType.Assign, firstValue, optionalSecondValue);
+                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, AttributeChangeType.Assign, value, null);
+        }
+
+        protected static void ChangingAttributeAssignElement(IGraphProcessingEnvironment procEnv, IGraphElement elem, AttributeType attrType, object value, object key)
+        {
+            if(elem is INode)
+                procEnv.Graph.ChangingNodeAttribute((INode)elem, attrType, AttributeChangeType.AssignElement, value, key);
+            else
+                procEnv.Graph.ChangingEdgeAttribute((IEdge)elem, attrType, AttributeChangeType.AssignElement, value, key);
         }
 
         #endregion helper methods
@@ -348,7 +356,7 @@ namespace de.unika.ipd.grGen.libGr
             value = ContainerHelper.IfAttributeOfElementIsContainerThenCloneContainer(
                 elem, AttributeName, value, out attrType);
 
-            ChangingAttributeAssignElement(procEnv, elem, attrType, value, null);
+            ChangingAttributeAssign(procEnv, elem, attrType, value);
 
             elem.SetAttribute(AttributeName, value);
 