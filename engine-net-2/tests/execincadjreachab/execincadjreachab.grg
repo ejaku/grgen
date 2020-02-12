ejaku/grgen

commit a04038aefc53d43c29bc3eda5dbc441cf12700f1

split EmitSequenceForReachable out of EmitSequenceForFunction

engine-net-2/src/lgspBackend/SequenceGenerator/SequenceGenerator.cs
@@ -703,11 +703,10 @@ namespace de.unika.ipd.grGen.lgsp
             SequenceExpression AdjacentNodeType = seqFor.ArgExprs.Count >= 3 ? seqFor.ArgExprs[2] : null;
             string adjacentNodeTypeExpr = helper.ExtractNodeType(source, AdjacentNodeType);
 
-            string iterationVariable; // valid for incident/adjacent and reachable
+            string iterationVariable;
             string iterationType;
-            string edgeMethod = null; // only valid for incident/adajcent
-            string theOther = null; // only valid for incident/adjacent
-            string reachableMethod = null; // only valid for reachable
+            string edgeMethod;
+            string theOther;
             switch(seqFor.SequenceType)
             {
             case SequenceType.ForAdjacentNodes:
@@ -746,6 +745,65 @@ namespace de.unika.ipd.grGen.lgsp
                 iterationVariable = "edge_" + seqFor.Id;
                 iterationType = IncidentEdgeType != null ? IncidentEdgeType.Type(env) : "AEdge";
                 break;
+            default:
+                edgeMethod = theOther = iterationVariable = iterationType = "INTERNAL ERROR";
+                break;
+            }
+
+            string profilingArgument = emitProfiling ? ", procEnv" : "";
+            if(emitProfiling)
+            {
+                source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge edge_{0} in node_{0}.{1})\n",
+                    seqFor.Id, edgeMethod);
+            }
+            else
+            {
+                source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge edge_{0} in node_{0}.GetCompatible{1}({2}))\n",
+                    seqFor.Id, edgeMethod, incidentEdgeTypeExpr);
+            }
+            source.AppendFront("{\n");
+            source.Indent();
+
+            if(emitProfiling)
+            {
+                source.AppendFront("++procEnv.PerformanceInfo.SearchSteps;\n");
+                source.AppendFrontFormat("if(!edge_{0}.InstanceOf(", seqFor.Id);
+                source.Append(incidentEdgeTypeExpr);
+                source.Append("))\n");
+                source.AppendFront("\tcontinue;\n");
+            }
+
+            // incident/adjacent needs a check for adjacent node, cause only incident edge can be type constrained in the loop
+            source.AppendFrontFormat("if(!{0}.InstanceOf({1}))\n",
+                theOther, adjacentNodeTypeExpr);
+            source.AppendFront("\tcontinue;\n");
+
+            source.AppendFront(helper.SetVar(seqFor.Var, iterationVariable));
+
+            EmitSequence(seqFor.Seq, source);
+
+            source.AppendFront(compGen.SetResultVar(seqFor, compGen.GetResultVar(seqFor) + " & " + compGen.GetResultVar(seqFor.Seq)));
+            source.Unindent();
+            source.AppendFront("}\n");
+        }
+
+        private void EmitSequenceForReachable(SequenceForFunction seqFor, SourceBuilder source)
+        {
+            source.AppendFront(compGen.SetResultVar(seqFor, "true"));
+
+            string sourceNodeExpr = exprGen.GetSequenceExpression(seqFor.ArgExprs[0], source);
+            source.AppendFrontFormat("GRGEN_LIBGR.INode node_{0} = (GRGEN_LIBGR.INode)({1});\n", seqFor.Id, sourceNodeExpr);
+
+            SequenceExpression IncidentEdgeType = seqFor.ArgExprs.Count >= 2 ? seqFor.ArgExprs[1] : null;
+            string incidentEdgeTypeExpr = helper.ExtractEdgeType(source, IncidentEdgeType);
+            SequenceExpression AdjacentNodeType = seqFor.ArgExprs.Count >= 3 ? seqFor.ArgExprs[2] : null;
+            string adjacentNodeTypeExpr = helper.ExtractNodeType(source, AdjacentNodeType);
+
+            string iterationVariable;
+            string iterationType;
+            string reachableMethod;
+            switch(seqFor.SequenceType)
+            {
             case SequenceType.ForReachableNodes:
                 reachableMethod = "";
                 iterationVariable = "iter_" + seqFor.Id; ;
@@ -777,7 +835,7 @@ namespace de.unika.ipd.grGen.lgsp
                 iterationType = IncidentEdgeType != null ? IncidentEdgeType.Type(env) : "AEdge";
                 break;
             default:
-                edgeMethod = theOther = iterationVariable = iterationType = "INTERNAL ERROR";
+                reachableMethod = iterationVariable = iterationType = "INTERNAL ERROR";
                 break;
             }
 
@@ -787,45 +845,13 @@ namespace de.unika.ipd.grGen.lgsp
                 source.AppendFrontFormat("foreach(GRGEN_LIBGR.INode iter_{0} in GraphHelper.Reachable{1}(node_{2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
                     seqFor.Id, reachableMethod, seqFor.Id, incidentEdgeTypeExpr, adjacentNodeTypeExpr);
             }
-            else if(seqFor.SequenceType == SequenceType.ForReachableEdges || seqFor.SequenceType == SequenceType.ForReachableEdgesViaIncoming || seqFor.SequenceType == SequenceType.ForReachableEdgesViaOutgoing)
+            else //if(seqFor.SequenceType == SequenceType.ForReachableEdges || seqFor.SequenceType == SequenceType.ForReachableEdgesViaIncoming || seqFor.SequenceType == SequenceType.ForReachableEdgesViaOutgoing)
             {
                 source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge edge_{0} in GraphHelper.Reachable{1}(node_{2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
                     seqFor.Id, reachableMethod, seqFor.Id, incidentEdgeTypeExpr, adjacentNodeTypeExpr);
             }
-            else
-            {
-                if(emitProfiling)
-                {
-                    source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge edge_{0} in node_{0}.{1})\n",
-                        seqFor.Id, edgeMethod);
-                }
-                else
-                {
-                    source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge edge_{0} in node_{0}.GetCompatible{1}({2}))\n",
-                        seqFor.Id, edgeMethod, incidentEdgeTypeExpr);
-                }
-            }
             source.AppendFront("{\n");
             source.Indent();
-
-            if(seqFor.SequenceType != SequenceType.ForReachableNodes && seqFor.SequenceType != SequenceType.ForReachableNodesViaIncoming && seqFor.SequenceType != SequenceType.ForReachableNodesViaOutgoing
-                && seqFor.SequenceType != SequenceType.ForReachableEdges && seqFor.SequenceType != SequenceType.ForReachableEdgesViaIncoming || seqFor.SequenceType != SequenceType.ForReachableEdgesViaOutgoing)
-            {
-                if(emitProfiling)
-                {
-                    source.AppendFront("++procEnv.PerformanceInfo.SearchSteps;\n");
-                    source.AppendFrontFormat("if(!edge_{0}.InstanceOf(", seqFor.Id);
-                    source.Append(incidentEdgeTypeExpr);
-                    source.Append("))\n");
-                    source.AppendFront("\tcontinue;\n");
-                }
-
-                // incident/adjacent needs a check for adjacent node, cause only incident edge can be type constrained in the loop
-                // reachable already allows to iterate exactly the edges of interest
-                source.AppendFrontFormat("if(!{0}.InstanceOf({1}))\n",
-                    theOther, adjacentNodeTypeExpr);
-                source.AppendFront("\tcontinue;\n");
-            }
 
             source.AppendFront(helper.SetVar(seqFor.Var, iterationVariable));
 
@@ -892,7 +918,7 @@ namespace de.unika.ipd.grGen.lgsp
                 source.AppendFrontFormat("foreach(GRGEN_LIBGR.INode {0} in GRGEN_LIBGR.GraphHelper.BoundedReachable{1}({2}, {3}, ({4}), ({5}), graph" + profilingArgument + "))\n",
                     iterationVariable, reachableMethod, sourceNodeName, depthVarName, incidentEdgeTypeExpr, adjacentNodeTypeExpr);
             }
-            else if(seqFor.SequenceType == SequenceType.ForBoundedReachableEdges || seqFor.SequenceType == SequenceType.ForBoundedReachableEdgesViaIncoming || seqFor.SequenceType == SequenceType.ForBoundedReachableEdgesViaOutgoing)
+            else //if(seqFor.SequenceType == SequenceType.ForBoundedReachableEdges || seqFor.SequenceType == SequenceType.ForBoundedReachableEdgesViaIncoming || seqFor.SequenceType == SequenceType.ForBoundedReachableEdgesViaOutgoing)
             {
                 source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge {0} in GRGEN_LIBGR.GraphHelper.BoundedReachable{1}({2}, {3}, ({4}), ({5}), graph" + profilingArgument + "))\n",
                     iterationVariable, reachableMethod, sourceNodeName, depthVarName, incidentEdgeTypeExpr, adjacentNodeTypeExpr);

commit 74d0c939a8fe23ed3e1f53d49dbc9c17b8950ce5

- fixes for bug introduced in commit a04038ae from 11.02.20 (missing call of new function) and old standing bug (missing namespace prefix) - added semantic tests (for-function-loops in sequences were not covered)

engine-net-2/src/lgspBackend/SequenceGenerator/SequenceGenerator.cs
@@ -109,13 +109,16 @@ namespace de.unika.ipd.grGen.lgsp
             case SequenceType.ForIncidentEdges:
             case SequenceType.ForIncomingEdges:
             case SequenceType.ForOutgoingEdges:
+                EmitSequenceForFunction((SequenceForFunction)seq, source);
+                break;
+
             case SequenceType.ForReachableNodes:
             case SequenceType.ForReachableNodesViaIncoming:
             case SequenceType.ForReachableNodesViaOutgoing:
             case SequenceType.ForReachableEdges:
             case SequenceType.ForReachableEdgesViaIncoming:
             case SequenceType.ForReachableEdgesViaOutgoing:
-                EmitSequenceForFunction((SequenceForFunction)seq, source);
+                EmitSequenceForReachable((SequenceForFunction)seq, source);
                 break;
 
             case SequenceType.ForBoundedReachableNodes:
@@ -852,12 +855,12 @@ namespace de.unika.ipd.grGen.lgsp
             string profilingArgument = emitProfiling ? ", procEnv" : "";
             if(seqFor.SequenceType == SequenceType.ForReachableNodes || seqFor.SequenceType == SequenceType.ForReachableNodesViaIncoming || seqFor.SequenceType == SequenceType.ForReachableNodesViaOutgoing)
             {
-                source.AppendFrontFormat("foreach(GRGEN_LIBGR.INode {0} in GraphHelper.Reachable{1}({2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
+                source.AppendFrontFormat("foreach(GRGEN_LIBGR.INode {0} in GRGEN_LIBGR.GraphHelper.Reachable{1}({2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
                     iterationVariable, reachableMethod, sourceNodeName, incidentEdgeTypeExpr, adjacentNodeTypeExpr);
             }
             else //if(seqFor.SequenceType == SequenceType.ForReachableEdges || seqFor.SequenceType == SequenceType.ForReachableEdgesViaIncoming || seqFor.SequenceType == SequenceType.ForReachableEdgesViaOutgoing)
             {
-                source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge {0} in GraphHelper.Reachable{1}({2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
+                source.AppendFrontFormat("foreach(GRGEN_LIBGR.IEdge {0} in GRGEN_LIBGR.GraphHelper.Reachable{1}({2}, ({3}), ({4}), graph" + profilingArgument + "))\n",
                     iterationVariable, reachableMethod, sourceNodeName, incidentEdgeTypeExpr, adjacentNodeTypeExpr);
             }
             source.AppendFront("{\n");

engine-net-2/tests/execincadjreachab/adjacentincident.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in adjacentIncoming(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in incoming(::s,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in adjacentOutgoing(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in outgoing(::s,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in adjacent(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in incident(::s,Edge,N); {::a.add(e)}}
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/adjacentincident.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n1]"
+1
+1
+0
+0
+"[e2,e1]"
+1
+1
+0
+0
+"[n2,n1,n4]"
+1
+1
+0
+0
+"[e2,e1,e4]"

engine-net-2/tests/execincadjreachab/adjacentincidentcompiled.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec forAdjacentIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forAdjacentOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forAdjacent
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forIncident
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/adjacentincidentcompiled.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n1]"
+1
+1
+0
+0
+"[e2,e1]"
+1
+1
+0
+0
+"[n2,n1,n4]"
+1
+1
+0
+0
+"[e2,e1,e4]"

engine-net-2/tests/execincadjreachab/boundedreachable.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in boundedReachableIncoming(::s,1,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdgesIncoming(::s,1,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in boundedReachableOutgoing(::s,1,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdgesOutgoing(::s,1,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in boundedReachable(::s,1,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdges(::s,1,Edge,N); {::a.add(e)}}
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/boundedreachable.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n1]"
+1
+1
+0
+0
+"[e2,e1]"
+1
+1
+0
+0
+"[n2,n1,n4]"
+1
+1
+0
+0
+"[e2,e1,e4]"

engine-net-2/tests/execincadjreachab/boundedreachablecompiled.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec forBoundedReachableIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forBoundedReachableEdgesIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forBoundedReachableOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forBoundedReachableEdgesOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forBoundedReachable
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forBoundedReachableEdges
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/boundedreachablecompiled.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n1]"
+1
+1
+0
+0
+"[e2,e1]"
+1
+1
+0
+0
+"[n2,n1,n4]"
+1
+1
+0
+0
+"[e2,e1,e4]"

engine-net-2/tests/execincadjreachab/execincadjreachab.gm
@@ -1,0 +1,5 @@
+node class Start;
+node class N;
+node class M;
+
+edge class E;

engine-net-2/tests/execincadjreachab/execincadjreachab.grg
@@ -1,0 +1,116 @@
+#using "execincadjreachab.gm"
+
+rule init : (Start)
+{
+	modify {
+		s:Start@($="start") -:E@($="e1")-> :N@($="n1") -:Edge@($="d1")-> :M@($="m1");
+		s -:E@($="e2")-> :N@($="n2") -:E@($="e3")-> :N@($="n3");
+		s -:Edge@($="d2")-> :M@($="m2");
+		s <-:E@($="e4")- :N@($="n4");
+		return(s);
+	}
+}
+
+
+sequence forNodes
+{
+	::a=array<Node>[] ;> for{n:Node in nodes(N); {::a.add(n)}}
+}
+
+sequence forEdges
+{
+	::a=array<Edge>[] ;> for{e:Edge in edges(E); {::a.add(e)}}
+}
+
+
+sequence forAdjacentIncoming
+{
+	::a=array<Node>[] ;> for{n:Node in adjacentIncoming(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forIncoming
+{
+	::a=array<Edge>[] ;> for{e:Edge in incoming(::s,Edge,N); {::a.add(e)}}
+}
+
+sequence forAdjacentOutgoing
+{
+	::a=array<Node>[] ;> for{n:Node in adjacentOutgoing(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forOutgoing
+{
+	::a=array<Edge>[] ;> for{e:Edge in outgoing(::s,Edge,N); {::a.add(e)}}
+}
+
+sequence forAdjacent
+{
+	::a=array<Node>[] ;> for{n:Node in adjacent(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forIncident
+{
+	::a=array<Edge>[] ;> for{e:Edge in incident(::s,Edge,N); {::a.add(e)}}
+}
+
+
+sequence forReachableIncoming
+{
+	::a=array<Node>[] ;> for{n:Node in reachableIncoming(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forReachableEdgesIncoming
+{
+	::a=array<Edge>[] ;> for{e:Edge in reachableEdgesIncoming(::s,Edge,N); {::a.add(e)}}
+}
+
+sequence forReachableOutgoing
+{
+	::a=array<Node>[] ;> for{n:Node in reachableOutgoing(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forReachableEdgesOutgoing
+{
+	::a=array<Edge>[] ;> for{e:Edge in reachableEdgesOutgoing(::s,Edge,N); {::a.add(e)}}
+}
+
+sequence forReachable
+{
+	::a=array<Node>[] ;> for{n:Node in reachable(::s,Edge,N); {::a.add(n)}}
+}
+
+sequence forReachableEdges
+{
+	::a=array<Edge>[] ;> for{e:Edge in reachableEdges(::s,Edge,N); {::a.add(e)}}
+}
+
+
+sequence forBoundedReachableIncoming
+{
+	::a=array<Node>[] ;> for{n:Node in boundedReachableIncoming(::s,1,Edge,N); {::a.add(n)}}
+}
+
+sequence forBoundedReachableEdgesIncoming
+{
+	::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdgesIncoming(::s,1,Edge,N); {::a.add(e)}}
+}
+
+sequence forBoundedReachableOutgoing
+{
+	::a=array<Node>[] ;> for{n:Node in boundedReachableOutgoing(::s,1,Edge,N); {::a.add(n)}}
+}
+
+sequence forBoundedReachableEdgesOutgoing
+{
+	::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdgesOutgoing(::s,1,Edge,N); {::a.add(e)}}
+}
+
+sequence forBoundedReachable
+{
+	::a=array<Node>[] ;> for{n:Node in boundedReachable(::s,1,Edge,N); {::a.add(n)}}
+}
+
+sequence forBoundedReachableEdges
+{
+	::a=array<Edge>[] ;> for{e:Edge in boundedReachableEdges(::s,1,Edge,N); {::a.add(e)}}
+}

engine-net-2/tests/execincadjreachab/nodesedges.grs
@@ -1,0 +1,13 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in nodes(N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in edges(E); {::a.add(e)}}
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/nodesedges.grs.data
@@ -1,0 +1,10 @@
+1
+1
+0
+0
+"[n4,n3,n2,n1]"
+1
+1
+0
+0
+"[e4,e3,e2,e1]"

engine-net-2/tests/execincadjreachab/nodesedgescompiled.grs
@@ -1,0 +1,13 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec forNodes
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forEdges
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/nodesedgescompiled.grs.data
@@ -1,0 +1,10 @@
+1
+1
+0
+0
+"[n4,n3,n2,n1]"
+1
+1
+0
+0
+"[e4,e3,e2,e1]"

engine-net-2/tests/execincadjreachab/reachable.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in reachableIncoming(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in reachableEdgesIncoming(::s,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in reachableOutgoing(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in reachableEdgesOutgoing(::s,Edge,N); {::a.add(e)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Node>[] ;> for{n:Node in reachable(::s,Edge,N); {::a.add(n)}}
+show var a
+
+clear graph
+exec (::s)=init
+
+exec ::a=array<Edge>[] ;> for{e:Edge in reachableEdges(::s,Edge,N); {::a.add(e)}}
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/reachable.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n3,n1]"
+1
+1
+0
+0
+"[e2,e3,e1]"
+1
+1
+0
+0
+"[n2,n3,n1,n4]"
+1
+1
+0
+0
+"[e2,e3,e3,e1,e4]"

engine-net-2/tests/execincadjreachab/reachablecompiled.grs
@@ -1,0 +1,37 @@
+new graph execincadjreachab/execincadjreachab
+exec (::s)=init
+
+exec forReachableIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forReachableEdgesIncoming
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forReachableOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forReachableEdgesOutgoing
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forReachable
+show var a
+
+clear graph
+exec (::s)=init
+
+exec forReachableEdges
+show var a
+
+quit

engine-net-2/tests/execincadjreachab/reachablecompiled.grs.data
@@ -1,0 +1,30 @@
+1
+1
+0
+0
+"[n4]"
+1
+1
+0
+0
+"[e4]"
+1
+1
+0
+0
+"[n2,n3,n1]"
+1
+1
+0
+0
+"[e2,e3,e1]"
+1
+1
+0
+0
+"[n2,n3,n1,n4]"
+1
+1
+0
+0
+"[e2,e3,e3,e1,e4]"