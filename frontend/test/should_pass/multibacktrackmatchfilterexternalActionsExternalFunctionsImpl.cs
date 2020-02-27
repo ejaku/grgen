30f1888f6f72546303206ffda06d360786cce68c
eja <spamja@arcor.de>
2020-02-27 09:51:46 +0100

    added frontend tests for external match class filter functions

---
+++ frontend/test/should_pass/multibacktrackmatchfilterexternalActionsExternalFunctionsImpl.cs
@@ -0,0 +1,25 @@
+// This file has been generated automatically by GrGen (www.grgen.net)
+// Do not modify this file! Any changes will be lost!
+// Generated from "multibacktrackmatchfilterexternal.grg" on 27.02.2020 11:36:02 Mitteleuropäische Zeit
+using System;
+using System.Collections.Generic;
+using GRGEN_LIBGR = de.unika.ipd.grGen.libGr;
+using GRGEN_LGSP = de.unika.ipd.grGen.lgsp;
+using GRGEN_MODEL = de.unika.ipd.grGen.Model_model;
+using GRGEN_ACTIONS = de.unika.ipd.grGen.Action_multibacktrackmatchfilterexternal;
+
+namespace de.unika.ipd.grGen.Action_multibacktrackmatchfilterexternal
+{
+    // ------------------------------------------------------
+
+    // You must implement the following match class filter functions in the same partial class in ./multibacktrackmatchfilterexternalActionsExternalFunctionsImpl.cs
+
+    public partial class MatchClassFilters
+    {
+        public static void Filter_shfext(GRGEN_LGSP.LGSPGraphProcessingEnvironment procEnv, IList<GRGEN_LIBGR.IMatch> matches, System.Int32 f)
+		{
+		}
+    }
+
+    // ------------------------------------------------------
+}
---
+++ frontend/test/should_pass/multiruleallcallmatchfilterexternalActionsExternalFunctionsImpl.cs
@@ -0,0 +1,23 @@
+// This file has been generated automatically by GrGen (www.grgen.net)
+// Do not modify this file! Any changes will be lost!
+// Generated from "multiruleallcallmatchfilterexternal.grg" on 27.02.2020 09:47:22 Mitteleuropäische Zeit
+using System;
+using System.Collections.Generic;
+using GRGEN_LIBGR = de.unika.ipd.grGen.libGr;
+using GRGEN_LGSP = de.unika.ipd.grGen.lgsp;
+using GRGEN_MODEL = de.unika.ipd.grGen.Model_model;
+using GRGEN_ACTIONS = de.unika.ipd.grGen.Action_multiruleallcallmatchfilterexternal;
+
+namespace de.unika.ipd.grGen.Action_multiruleallcallmatchfilterexternal
+{
+    // ------------------------------------------------------
+
+    public partial class MatchClassFilters
+    {
+        public static void Filter_shfext(GRGEN_LGSP.LGSPGraphProcessingEnvironment procEnv, IList<GRGEN_LIBGR.IMatch> matches, System.Int32 f)
+        {
+        }
+    }
+
+    // ------------------------------------------------------
+}
