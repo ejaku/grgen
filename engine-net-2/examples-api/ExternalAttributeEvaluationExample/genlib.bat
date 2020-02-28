4cc1d41722f1ce13dfab9eee4f12320d3d1e72ab
eja <spamja@arcor.de>
2020-02-29 00:01:17 +0100

    extended api example generation helper scripts to copy the external functions (generated out-of-place)

--- engine-net-2/examples-api/ExternalAttributeEvaluationExample/genlib.bat
+++ engine-net-2/examples-api/ExternalAttributeEvaluationExample/genlib.bat
@@ -1,2 +1,3 @@
 ..\..\bin\grgen -keep . ..\..\examples\ExternalAttributeEvaluationExample\ExternalAttributeEvaluation.grg
+copy ..\..\examples\ExternalAttributeEvaluationExample\ExternalAttributeEvaluationModelExternalFunctions.cs ..\..\examples-api\ExternalAttributeEvaluationExample\ExternalAttributeEvaluationModelExternalFunctions.cs
 @if ERRORLEVEL 1 PAUSE
--- engine-net-2/examples-api/ExternalFiltersAndSequencesExample/genlib.bat
+++ engine-net-2/examples-api/ExternalFiltersAndSequencesExample/genlib.bat
@@ -1,2 +1,3 @@
 ..\..\bin\grgen -keep . ..\..\examples\ExternalFiltersAndSequencesExample\ExternalFiltersAndSequences.grg
+copy ..\..\examples\ExternalFiltersAndSequencesExample\ExternalFiltersAndSequencesActionsExternalFunctions.cs ..\..\examples-api\ExternalFiltersAndSequencesExample\ExternalFiltersAndSequencesActionsExternalFunctions.cs
 @if ERRORLEVEL 1 PAUSE
