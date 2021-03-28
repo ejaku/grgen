021a3fcdb87aec7029766917e4e9ef2106b7b491
eja <spamja@arcor.de>
2021-03-28 10:01:21 +0200

    changed file headers to 6.0

--- engine-net-2/src/lgspBackend/Properties/AssemblyInfo.cs
+++ engine-net-2/src/lgspBackend/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * GrGen: graph rewrite generator tool -- release GrGen.NET 5.2
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 6.0
  * Copyright (C) 2003-2021 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
  * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
  * www.grgen.net
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.2.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("GrGen.NET 5.2")]
-[assembly: AssemblyFileVersion("5.2.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("GrGen.NET 6.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
