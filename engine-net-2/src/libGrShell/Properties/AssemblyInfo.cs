30a19bcd38ce202daebb4bfda9f01600e709de41
eja <spamja@arcor.de>
2022-02-05 11:59:24 +0100

    split a libGrShell library project out of the GrShell application project   (the former can be included in an own application project in order to gain GrShell-like functionality/graph rewrite shell script interpretation ability, the latter consists only of the Main function anymore, referencing the library)

---
+++ engine-net-2/src/libGrShell/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 6.5
+ * Copyright (C) 2003-2022 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyTitle("libGrShell")]
+[assembly: AssemblyProduct("libGrShell")]
+[assembly: AssemblyCompany("Universität Karlsruhe, IPD Goos")]
+[assembly: AssemblyCopyright("Copyright © 2003-2022 Universität Karlsruhe, IPD Goos; and free programmers")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c723345-5cee-4b82-b69c-6d7954e552f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("6.5.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("GrGen.NET 6.5")]
+[assembly: AssemblyFileVersion("6.5.0.0")]
