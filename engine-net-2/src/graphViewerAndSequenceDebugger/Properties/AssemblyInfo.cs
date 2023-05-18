dc21de96d332f5704ba0a110818af6645513dbf3
eja <spamja@arcor.de>
2023-05-18 13:27:18 +0200

    missing file header /adaptation

--- engine-net-2/src/graphViewerAndSequenceDebugger/Properties/AssemblyInfo.cs
+++ engine-net-2/src/graphViewerAndSequenceDebugger/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
-﻿using System.Reflection;
+﻿/*
+ * GrGen: graph rewrite generator tool -- release GrGen.NET 6.7
+ * Copyright (C) 2003-2023 Universitaet Karlsruhe, Institut fuer Programmstrukturen und Datenorganisation, LS Goos; and free programmers
+ * licensed under LGPL v3 (see LICENSE.txt included in the packaging of this file)
+ * www.grgen.net
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("graphViewerAndSequenceDebugger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Universität Karlsruhe, IPD Goos")]
-[assembly: AssemblyProduct("graphViewerAndSequenceDebugger")]
-[assembly: AssemblyCopyright("Copyright © 2003-2022 Universität Karlsruhe, IPD Goos; and free programmers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: AssemblyTitle("graphViewerAndSequenceDebugger")]
+[assembly: AssemblyCompany("Universität Karlsruhe, IPD Goos")]
+[assembly: AssemblyProduct("graphViewerAndSequenceDebugger")]
+[assembly: AssemblyCopyright("Copyright © 2003-2023 Universität Karlsruhe, IPD Goos; and free programmers")]
 
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
@@ -32,5 +40,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("6.7.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("GrGen.NET 6.7")]
+[assembly: AssemblyFileVersion("6.7.0.0")]
