ejaku/grgen

commit 7748edfbc695aaef9bba7578dfbb690601de48d4

- removed renaming of retyped elements (in the graph viewer client, from the name of the old to the name of the new element, the names are the same since a while now) - removed temporary renaming of deleted elements to "zombie_" + name before finally purging them (in the debugger and changes recorder) - the RenameNode/RenameEdge functions of the IBasicGraphViewerClient are now unused and deprecated - undo of the commit: workaround for failing node(/edge) deletion in the MSAGL graph viewer

engine-net-2/src/graphViewerAndSequenceDebugger/Debugger.cs
@@ -1215,8 +1215,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 graphViewerClient.ChangeNode(node, realizers.DeletedNodeRealizer);
 
                 String name = graphViewerClient.Graph.GetElementName(node);
-                graphViewerClient.RenameNode(name, "zombie_" + name);
-                renderRecorder.DeletedNode("zombie_" + name, name);
+                renderRecorder.DeletedNode(name); // rename to zombie_ + name removed
             }
         }
 
@@ -1244,8 +1243,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 graphViewerClient.ChangeEdge(edge, realizers.DeletedEdgeRealizer);
 
                 String name = graphViewerClient.Graph.GetElementName(edge);
-                graphViewerClient.RenameEdge(name, "zombie_" + name);
-                renderRecorder.DeletedEdge("zombie_" + name, name);
+                renderRecorder.DeletedEdge(name); // rename to zombie_ + name removed
             }
         }
 

engine-net-2/src/graphViewerAndSequenceDebugger/GraphAnnotationAndChangesRecorder.cs
@@ -36,8 +36,8 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
         private readonly Dictionary<INode, bool> addedNodes = new Dictionary<INode, bool>();
         private readonly Dictionary<IEdge, bool> addedEdges = new Dictionary<IEdge, bool>();
 
-        private readonly List<KeyValuePair<String, String>> deletedNodes = new List<KeyValuePair<String, String>>();
-        private readonly List<KeyValuePair<String, String>> deletedEdges = new List<KeyValuePair<String, String>>();
+        private readonly List<String> deletedNodes = new List<String>();
+        private readonly List<String> deletedEdges = new List<String>();
 
         private readonly Dictionary<INode, bool> retypedNodes = new Dictionary<INode, bool>();
         private readonly Dictionary<IEdge, bool> retypedEdges = new Dictionary<IEdge, bool>();
@@ -142,14 +142,14 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 return "newly added";
         }
 
-        public void DeletedNode(String nodeName, String oldNodeName)
+        public void DeletedNode(String nodeName)
         {
-            deletedNodes.Add(new KeyValuePair<String, String>(nodeName, oldNodeName)); // special handling for the zombie_ nodes -- GUI TODO: needed at all?
+            deletedNodes.Add(nodeName);
         }
 
-        public void DeletedEdge(String edgeName, String oldEdgeName)
+        public void DeletedEdge(String edgeName)
         {
-            deletedEdges.Add(new KeyValuePair<String, String>(edgeName, oldEdgeName)); // special handling for the zombie_ edges -- GUI TODO: needed at all?
+            deletedEdges.Add(edgeName);
         }
 
         public bool WasNodeAnnotationReplaced(INode oldNode, INode newNode, out String name)
@@ -208,13 +208,13 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 graphViewerClient.AnnotateElement(edge, null);
             }
 
-            foreach(KeyValuePair<String, String> edgeNames in deletedEdges)
+            foreach(String edgeName in deletedEdges)
             {
-                graphViewerClient.DeleteEdge(edgeNames.Key, edgeNames.Value);
+                graphViewerClient.DeleteEdge(edgeName);
             }
-            foreach(KeyValuePair<String, String> nodeNames in deletedNodes)
+            foreach(String nodeName in deletedNodes)
             {
-                graphViewerClient.DeleteNode(nodeNames.Key, nodeNames.Value);
+                graphViewerClient.DeleteNode(nodeName);
             }
 
             foreach(INode node in retypedNodes.Keys)

engine-net-2/src/graphViewerAndSequenceDebugger/GraphViewerClient.cs
@@ -721,18 +721,19 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                     ChangeEdge((IEdge)oldElem, newEr);
             }
 
-            String newName = graph.GetElementName(newElem);
-            if(isNode)
-                basicClient.RenameNode(oldName, newName);
-            else
-                basicClient.RenameEdge(oldName, newName);
+            // retyping keeps the name constant (was maybe different somewhen in the past), so this code is not needed (anymore)
+            //String newName = graph.GetElementName(newElem);
+            //if(isNode)
+            //    basicClient.RenameNode(oldName, newName);
+            //else
+            //    basicClient.RenameEdge(oldName, newName);
 
             isDirty = true;
         }
 
-        public void DeleteNode(String nodeName, String oldNodeName)
+        public void DeleteNode(String nodeName)
         {
-            basicClient.DeleteNode(nodeName, oldNodeName);
+            basicClient.DeleteNode(nodeName);
             isDirty = true;
             isLayoutDirty = true;
         }
@@ -742,14 +743,14 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             if(IsNodeExcluded(node))
                 return;
 
-            DeleteNode(graph.GetElementName(node), null);
+            DeleteNode(graph.GetElementName(node));
         }
 
-        public void DeleteEdge(String edgeName, String oldEdgeName)
+        public void DeleteEdge(String edgeName)
         {
             // TODO: Update group relation
 
-            basicClient.DeleteEdge(edgeName, oldEdgeName);
+            basicClient.DeleteEdge(edgeName);
             isDirty = true;
             isLayoutDirty = true;
         }
@@ -763,18 +764,18 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             if(IsEdgeExcluded(edge))
                 return;
 
-            DeleteEdge(graph.GetElementName(edge), null);
+            DeleteEdge(graph.GetElementName(edge));
         }
 
-        public void RenameNode(String oldName, String newName)
-        {
-            basicClient.RenameNode(oldName, newName);
-        }
+        //public void RenameNode(String oldName, String newName) // TODO: purge
+        //{
+        //    basicClient.RenameNode(oldName, newName);
+        //}
 
-        public void RenameEdge(String oldName, String newName)
-        {
-            basicClient.RenameEdge(oldName, newName);
-        }
+        //public void RenameEdge(String oldName, String newName) // TODO: purge
+        //{
+        //    basicClient.RenameEdge(oldName, newName);
+        //}
 
         /// <summary>
         /// Uploads the graph to YComp, updates the display and makes a synchronisation.

engine-net-2/src/graphViewerAndSequenceDebugger/Highlighter.cs
@@ -206,7 +206,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                     if(addAnnotation)
                         graphViewerClient.AddEdge(name + i, name + "[->]", (INode)value[i-1], (INode)value[i]);
                     else
-                        graphViewerClient.DeleteEdge(name + i, null);
+                        graphViewerClient.DeleteEdge(name + i);
                 }
             }
         }
@@ -224,7 +224,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                     if(addAnnotation)
                         graphViewerClient.AddEdge(name + distanceToTop, name + "[->]", (INode)prevElem, (INode)elem);
                     else
-                        graphViewerClient.DeleteEdge(name + distanceToTop, null);
+                        graphViewerClient.DeleteEdge(name + distanceToTop);
                 }
                 prevElem = elem;
                 ++distanceToTop;
@@ -238,7 +238,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             if(addAnnotation)
                 graphViewerClient.AddEdge(name + cnt, name, source, target);
             else
-                graphViewerClient.DeleteEdge(name + cnt, null);
+                graphViewerClient.DeleteEdge(name + cnt);
         }
 
         private void HighlightSingleValue(object value, string name, bool addAnnotation)

engine-net-2/src/graphViewerAndSequenceDebugger/IBasicGraphViewerClient.cs
@@ -89,13 +89,13 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         void ClearEdgeAttribute(String name, String ownerTypeName, String attrTypeName, String attrTypeString);
 
-        void DeleteNode(String nodeName, String oldNodeName); // only nodeName required - scruffy hack - it seems MSAGL doesn't update its internal data structures if the id/name of a graph element is changed - this allows to implement the temporary zombie_ + old-name handling of deleted nodes/edges in the graph viewer for rule rewrite debugging
+        void DeleteNode(String nodeName);
 
-        void DeleteEdge(String edgeName, String oldEdgeName); // only edgeName required - scruffy hack - it seems MSAGL doesn't update its internal data structures if the id/name of a graph element is changed - this allows to implement the temporary zombie_ + old-name handling of deleted nodes/edges in the graph viewer for rule rewrite debugging
+        void DeleteEdge(String edgeName);
 
-        void RenameNode(String oldName, String newName); // this does't work with MSAGL it seems, don't use it / rely on it - GUI TODO: is this needed at all?
+        void RenameNode(String oldName, String newName); // deprecated - used in the past (for retyping, zombie_-names of deleted elements), not needed anymore (esp. due to the current behavior of the named graph), does't work with MSAGL, you have to take care of this if you want to use it again
 
-        void RenameEdge(String oldName, String newName); // this does't work with MSAGL it seems, don't use it / rely on it - GUI TODO: is this needed at all?
+        void RenameEdge(String oldName, String newName); // deprecated - used in the past (for retyping, zombie_-names of deleted elements), not needed anymore (esp. due to the current behavior of the named graph), does't work with MSAGL, you have to take care of this if you want to use it again
 
         void ClearGraph();
 

engine-net-2/src/graphViewerAndSequenceDebugger/YCompClient.cs
@@ -399,12 +399,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void AddSubgraphNode(String name, String nrName, String nodeLabel)
         {
-            ycompStream.Write("addSubgraphNode \"-1\" \"n" + name + "\" \"" + nrName + "\" \"" + nodeLabel + "\"\n");
+            ycompStream.Write("addSubgraphNode \"-1\" \"n" + name + "\" \"" + nrName + "\" \"" + nodeLabel + "\"\n"); // -1 is for subgraph parent, could be added directly instead of a later MoveNode
         }
 
         public void AddNode(String name, String nrName, String nodeLabel)
         {
-            ycompStream.Write("addNode \"-1\" \"n" + name + "\" \"" + nrName + "\" \"" + nodeLabel + "\"\n");
+            ycompStream.Write("addNode \"-1\" \"n" + name + "\" \"" + nrName + "\" \"" + nodeLabel + "\"\n"); // -1 is for subgraph parent, could be added directly instead of a later MoveNode
         }
 
         public void SetNodeAttribute(String name, String ownerTypeName, String attrTypeName, String attrTypeString, String attrValueString)
@@ -465,12 +465,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 + attrTypeString + "\"\n");
         }
 
-        public void DeleteNode(String nodeName, String oldNodeName)
+        public void DeleteNode(String nodeName)
         {
             ycompStream.Write("deleteNode \"n" + nodeName + "\"\n");
         }
 
-        public void DeleteEdge(String edgeName, String oldEdgeName)
+        public void DeleteEdge(String edgeName)
         {
             // TODO: Update group relation
             ycompStream.Write("deleteEdge \"e" + edgeName + "\"\n");
@@ -478,12 +478,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void RenameNode(String oldName, String newName)
         {
-            ycompStream.Write("renameNode \"n" + oldName + "\" \"n" + newName + "\"\n");
+            ycompStream.Write("renameNode \"n" + oldName + "\" \"n" + newName + "\"\n"); // changes the name(==id) by which the node is accessible
         }
 
         public void RenameEdge(String oldName, String newName)
         {
-            ycompStream.Write("renameEdge \"e" + oldName + "\" \"e" + newName + "\"\n");
+            ycompStream.Write("renameEdge \"e" + oldName + "\" \"e" + newName + "\"\n"); // changes the name(==id) by which the edge is accessible
         }
 
         public void ClearGraph()

engine-net-2/src/graphViewerAndSequenceDebuggerWindowsForms/MSAGLClient.cs
@@ -308,11 +308,9 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             // TODO - for edge name remove tooltip of kind E::a:T, used during retyping
         }
 
-        public void DeleteNode(String nodeName, String oldNodeName)
+        public void DeleteNode(String nodeName)
         {
             Node node = gViewer.Graph.FindNode(nodeName);
-            if(node == null)
-                node = gViewer.Graph.FindNode(oldNodeName);
 
             if(node != null)
             {
@@ -385,7 +383,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             }
         }
 
-        public void DeleteEdge(String edgeName, String oldEdgeName)
+        public void DeleteEdge(String edgeName)
         {
             // TODO: Update group relation // GUI TODO: it seems EdgeById does not work at all
             /*Edge edge = gViewer.Graph.EdgeById(edgeName);
@@ -400,13 +398,6 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 nameToEdge.Remove(edgeName);
                 edgeToName.Remove(edge);
             }
-            else if(nameToEdge.ContainsKey(oldEdgeName))
-            {
-                Edge edge = nameToEdge[oldEdgeName];
-                gViewer.Graph.RemoveEdge(edge);
-                nameToEdge.Remove(oldEdgeName);
-                edgeToName.Remove(edge);
-            }
             else
             {
                 throw new Exception("Unknown edge");
@@ -415,22 +406,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void RenameNode(String oldName, String newName)
         {
-            // deleted nodes are shown as zombie_oldName, rename for retyping is not needed anymore, the name is kept
-            Node node = gViewer.Graph.FindNode(oldName);
-            //node.Id = newName; only changes MSAGL internal attribute, but node map entry that seems to be used by FindNode is not changed -- maybe change manually // GUI TODO
-            //node.Attr.Id = newName; don't know what's the exact relationship to node.Id, but edges only have an Attr.Id
+            throw new NotImplementedException(); // the simple solution of just assigning node.Id or node.Attr.Id was not sufficient, but as it is not needed anymore, I just leave it like this (future TODO: purge RenameNode (alternative: implement it))
         }
 
         public void RenameEdge(String oldName, String newName)
         {
-            // deleted edges are shown as zombie_oldName, rename for retyping is not needed anymore, the name is kept
-            Edge edge;
-            if(nameToEdge.TryGetValue(oldName, out edge))
-            {
-                //nameToEdge.Remove(oldName); // GUI TODO
-                //nameToEdge.Add(newName, edge);
-                //edge.Attr.Id = newName; GUI TODO: can this be used instead of edgeToName? label itself could be set to another text string
-            }
+            throw new NotImplementedException(); // the simple solution of just assigning edge.Attr.Id was not sufficient, but as it is not needed anymore, I just leave it like this (future TODO: purge RenameEdge (alternative: implement it))
         }
 
         public void ClearGraph()

engine-net-2/tools/TestMSAGL/MSAGLTestHost.cs
@@ -1,13 +1,6 @@
 ﻿using de.unika.ipd.grGen.graphViewerAndSequenceDebugger;
 using de.unika.ipd.grGen.libGr;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TestMSAGL
@@ -101,30 +94,30 @@ namespace TestMSAGL
 
         private void buttonDeleteGroup_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteNode("group", "group");
+            graphViewer.DeleteNode("group");
             graphViewer.Show();
         }
 
         private void buttonDeleteNode_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteNode("node", "node");
+            graphViewer.DeleteNode("node");
             graphViewer.Show();
         }
         private void buttonDeleteNestedGroup_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteNode("nestedGroup", "nestedGroup");
+            graphViewer.DeleteNode("nestedGroup");
             graphViewer.Show();
         }
 
         private void buttonDeleteNestedNode_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteNode("nestedNode", "nestedNode");
+            graphViewer.DeleteNode("nestedNode");
             graphViewer.Show();
         }
 
         private void buttonDeleteNestedNestedNode_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteNode("nestedNestedNode", "nestedNestedNode");
+            graphViewer.DeleteNode("nestedNestedNode");
             graphViewer.Show();
         }
 
@@ -136,37 +129,37 @@ namespace TestMSAGL
 
         private void buttonDeleteE1_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e1", "e1");
+            graphViewer.DeleteEdge("e1");
             graphViewer.Show();
         }
 
         private void buttonDeleteE2_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e2", "e2");
+            graphViewer.DeleteEdge("e2");
             graphViewer.Show();
         }
 
         private void buttonDeleteE3_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e3", "e3");
+            graphViewer.DeleteEdge("e3");
             graphViewer.Show();
         }
 
         private void buttonDeleteE4_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e4", "e4");
+            graphViewer.DeleteEdge("e4");
             graphViewer.Show();
         }
 
         private void buttonDeleteE5_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e5", "e5");
+            graphViewer.DeleteEdge("e5");
             graphViewer.Show();
         }
 
         private void buttonDeleteE6_Click(object sender, EventArgs e)
         {
-            graphViewer.DeleteEdge("e6", "e6");
+            graphViewer.DeleteEdge("e6");
             graphViewer.Show();
         }
     }

commit 534bc125dfe22af44ccd27b36c606a0152248e1f

- fixed the debugger display of the redirection of edges (likely broken since commit 7748edfbc695aaef9bba7578dfbb690601de48d4, depending on the graph viewer even a crash) - added a mode "redirected" to the debug set edge mode command, e.g. "debug set edge mode redirected color purple" sets the color of redirected edges in the debugger to purple (default is pink) - extended the Visualization/Debugging example to allow manual testing of the new mode (during edge redirection)

engine-net-2/examples/Visualization/Debugging.grg
@@ -9,7 +9,7 @@ rule init
 
 rule r
 {
-	n:Node --> m:Node --> l:Node --> n;	
+	n:Node --> m:Node -e:Edge-> l:Node --> n;	
 ---
 	def var i:int = 0;
 	
@@ -17,6 +17,7 @@ rule r
 		delete(n);
 		:Node<m>;
 		l --> :Node;
+		m !<-e-! l;
 	}
 }
 

engine-net-2/examples/Visualization/Debugging.grs
@@ -4,11 +4,13 @@ debug set node mode matched color grey
 debug set node mode created color green
 debug set node mode deleted color red
 debug set node mode retyped color blue
+debug set node mode redirected color purple
 
 debug set edge mode matched color grey
 debug set edge mode created color green
 debug set edge mode deleted color red
 debug set edge mode retyped color blue
+debug set edge mode redirected color purple
 
 exec init
 

engine-net-2/src/graphViewerAndSequenceDebugger/Debugger.cs
@@ -1195,6 +1195,8 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             {
                 String edgeName = renderRecorder.AddedEdge(edge);
                 graphViewerClient.AnnotateElement(edge, edgeName);
+                if(renderRecorder.IsEdgeRedirected(edge))
+                    graphViewerClient.ChangeEdge(edge, realizers.RedirectedEdgeRealizer);
             }
             else if(alwaysShow)
                 graphViewerClient.UpdateDisplay();
@@ -1248,11 +1250,16 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             }
             else
             {
-                renderRecorder.RemoveEdgeAnnotation(edge);
-                graphViewerClient.ChangeEdge(edge, realizers.DeletedEdgeRealizer);
+                if(renderRecorder.IsEdgeRedirected(edge))
+                    graphViewerClient.DeleteEdge(edge); // redirection is handled by deletion and addition, but real deletion is deferred, so addition with same name would crash (and renaming elements is not working properly with MSAGL) (alternative in order to get old behaviour would be to add here a zombie edge with fake name just to be deleted)
+                else
+                {
+                    renderRecorder.RemoveEdgeAnnotation(edge);
+                    graphViewerClient.ChangeEdge(edge, realizers.DeletedEdgeRealizer);
 
-                String name = graphViewerClient.Graph.GetElementName(edge);
-                renderRecorder.DeletedEdge(name); // rename to zombie_ + name removed
+                    String name = graphViewerClient.Graph.GetElementName(edge);
+                    renderRecorder.DeletedEdge(name); // rename to zombie_ + name removed
+                }
             }
         }
 
@@ -1326,6 +1333,11 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 graphViewerClient.ChangeEdge(newEdge, realizers.RetypedEdgeRealizer);
                 renderRecorder.RetypedEdge(newEdge);
             }
+        }
+
+        public void DebugRedirectingEdge(IEdge edge)
+        {
+            renderRecorder.RedirectEdge(edge);
         }
 
         public void DebugSettingAddedNodeNames(string[] namesOfNodesAdded)

engine-net-2/src/graphViewerAndSequenceDebugger/DebuggerTask.cs
@@ -100,6 +100,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
                 debugger.DebugRetypingElement(oldElem, newElem);
         }
 
+        private void DebugRedirectingEdge(IEdge edge)
+        {
+            if(isActive)
+                debugger.DebugRedirectingEdge(edge);
+        }
+
         private void DebugSettingAddedNodeNames(string[] namesOfNodesAdded)
         {
             if(isActive)
@@ -252,6 +258,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             graph.OnChangedEdgeAttribute += DebugChangedEdgeAttribute;
             graph.OnRetypingNode += DebugRetypingElement;
             graph.OnRetypingEdge += DebugRetypingElement;
+            graph.OnRedirectingEdge += DebugRedirectingEdge;
             graph.OnSettingAddedNodeNames += DebugSettingAddedNodeNames;
             graph.OnSettingAddedEdgeNames += DebugSettingAddedEdgeNames;
         }
@@ -302,6 +309,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             graph.OnChangedEdgeAttribute -= DebugChangedEdgeAttribute;
             graph.OnRetypingNode -= DebugRetypingElement;
             graph.OnRetypingEdge -= DebugRetypingElement;
+            graph.OnRedirectingEdge -= DebugRedirectingEdge;
             graph.OnSettingAddedNodeNames -= DebugSettingAddedNodeNames;
             graph.OnSettingAddedEdgeNames -= DebugSettingAddedEdgeNames;
         }

engine-net-2/src/graphViewerAndSequenceDebugger/ElementRealizers.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 
 using de.unika.ipd.grGen.libGr;
+using de.unika.ipd.grGen.libConsoleAndOS;
 
 namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 {
@@ -78,7 +79,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
     public enum ElementMode
     {
-        Normal = 0, Matched = 1, Created = 2, Deleted = 3, Retyped = 4
+        Normal = 0, Matched = 1, Created = 2, Deleted = 3, Retyped = 4, Redirected = 5
     }
 
     /// <summary>
@@ -128,10 +129,14 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
         {
             get { return edgeRealizers[(int)ElementMode.Retyped].Name; }
         }
+        public String RedirectedEdgeRealizer
+        {
+            get { return edgeRealizers[(int)ElementMode.Redirected].Name; }
+        }
 
         // the realizers registered to yComp for display during debugging
         readonly NodeRealizer[] nodeRealizers = new NodeRealizer[5];
-        readonly EdgeRealizer[] edgeRealizers = new EdgeRealizer[5];
+        readonly EdgeRealizer[] edgeRealizers = new EdgeRealizer[6];
 
         // set with all the realizers registered to yComp
         readonly Dictionary<NodeRealizer, NodeRealizer> registeredNodeRealizers = new Dictionary<NodeRealizer, NodeRealizer>();
@@ -164,6 +169,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             edgeRealizers[(int)ElementMode.Created] = GetEdgeRealizer(GrColor.YellowGreen, GrColor.Black, 3, GrLineStyle.Continuous);
             edgeRealizers[(int)ElementMode.Deleted] = GetEdgeRealizer(GrColor.LightGrey, GrColor.Black, 3, GrLineStyle.Continuous);
             edgeRealizers[(int)ElementMode.Retyped] = GetEdgeRealizer(GrColor.Aquamarine, GrColor.Black, 3, GrLineStyle.Continuous);
+            edgeRealizers[(int)ElementMode.Redirected] = GetEdgeRealizer(GrColor.Pink, GrColor.Black, 3, GrLineStyle.Continuous);
         }
 
         public void RegisterGraphViewerClient(GraphViewerClient graphViewerClient)
@@ -214,6 +220,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void ChangeNodeColor(ElementMode mode, GrColor color)
         {
+            if(mode == ElementMode.Redirected)
+            {
+                ConsoleUI.errorOutWriter.WriteLine("The mode " + mode + " is not supported for nodes!");
+                return;
+            }
+
             nodeRealizers[(int)mode] = GetNodeRealizer(
                 color,
                 nodeRealizers[(int)mode].BorderColor,
@@ -223,6 +235,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void ChangeNodeBorderColor(ElementMode mode, GrColor borderColor)
         {
+            if(mode == ElementMode.Redirected)
+            {
+                ConsoleUI.errorOutWriter.WriteLine("The mode " + mode + " is not supported for nodes!");
+                return;
+            }
+
             nodeRealizers[(int)mode] = GetNodeRealizer(
                 nodeRealizers[(int)mode].Color,
                 borderColor,
@@ -232,6 +250,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void ChangeNodeTextColor(ElementMode mode, GrColor textColor)
         {
+            if(mode == ElementMode.Redirected)
+            {
+                ConsoleUI.errorOutWriter.WriteLine("The mode " + mode + " is not supported for nodes!");
+                return;
+            }
+
             nodeRealizers[(int)mode] = GetNodeRealizer(
                 nodeRealizers[(int)mode].Color,
                 nodeRealizers[(int)mode].BorderColor,
@@ -241,6 +265,12 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
 
         public void ChangeNodeShape(ElementMode mode, GrNodeShape shape)
         {
+            if(mode == ElementMode.Redirected)
+            {
+                ConsoleUI.errorOutWriter.WriteLine("The mode " + mode + " is not supported for nodes!");
+                return;
+            }
+
             nodeRealizers[(int)mode] = GetNodeRealizer(
                 nodeRealizers[(int)mode].Color,
                 nodeRealizers[(int)mode].BorderColor,

engine-net-2/src/graphViewerAndSequenceDebugger/GraphAnnotationAndChangesRecorder.cs
@@ -42,6 +42,8 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
         private readonly Dictionary<INode, bool> retypedNodes = new Dictionary<INode, bool>();
         private readonly Dictionary<IEdge, bool> retypedEdges = new Dictionary<IEdge, bool>();
 
+        private readonly Dictionary<IEdge, bool> redirectedEdges = new Dictionary<IEdge, bool>();
+
 
         public void AddNodeAnnotation(INode node, String name)
         {
@@ -184,6 +186,16 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             retypedEdges[edge] = true;
         }
 
+        public void RedirectEdge(IEdge edge)
+        {
+            redirectedEdges[edge] = true;
+        }
+
+        public bool IsEdgeRedirected(IEdge edge)
+        {
+            return redirectedEdges.ContainsKey(edge);
+        }
+
         public void ResetAllChangedElements()
         {
             addedNodes.Clear();
@@ -192,6 +204,7 @@ namespace de.unika.ipd.grGen.graphViewerAndSequenceDebugger
             retypedEdges.Clear();
             deletedEdges.Clear();
             deletedNodes.Clear();
+            redirectedEdges.Clear();
         }
 
         // applies changes recorded so far, leaving a graph without visible changes behind (e.g. no annotations)