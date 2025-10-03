fcdb767023ccd8c6f7775391986b9182f79665df
eja <spamja@arcor.de>
2025-10-03 16:26:53 +0200

    - set icon for GGrShell - added GGrShell to editbin.bat - increased shell version to 8.0beta

--- engine-net-2/editbin.bat
+++ engine-net-2/editbin.bat
@@ -2,6 +2,7 @@ rem for the following to work, you have to copy (/add to path) editbin.exe (and 
 
 rem support deeply nested structures, in pattern matching (GrShell for executing matchers), or in sequence compilation (GrGen), by increasing the max stack size available for the recursive calls
 editbin /stack:16777216,16760832 bin\GrShell.exe
+editbin /stack:16777216,16760832 bin\GGrShell.exe
 editbin /stack:16777216,16760832 bin\GrGen.exe
 
 rem create a 32bit version being able to support 3GB instead of just 2GB if main memory (somwhat obsolete now that 64bit rules)
