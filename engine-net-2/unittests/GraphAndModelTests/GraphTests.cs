02bde531ad7f6029160140de8828e52f63e9da5d
eja <spamja@arcor.de>
2026-02-12 11:08:34 +0100

    added a unittest folder to the solution, a unit-test project (NUnit-based), and a first unit test (serving as a cradle for AI implementation)

---
+++ engine-net-2/unittests/GraphAndModelTests/GraphTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace GraphAndModelTests
+{
+    public class GraphTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // AI todo: implement a basic test of the IGraph interface
+            Assert.Pass();
+        }
+    }
+}
