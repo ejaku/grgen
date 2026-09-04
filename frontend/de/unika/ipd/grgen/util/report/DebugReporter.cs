cb55a1e3d4ed5cf0bad44402d93a7f70321ad75b
eja <spamja@arcor.de>
2026-09-04 20:51:03 +0200

    completed the conversion of the DebugReporter from Java to C#

--- frontend/de/unika/ipd/grgen/util/report/DebugReporter.cs
+++ frontend/de/unika/ipd/grgen/util/report/DebugReporter.cs
@@ -6,38 +6,31 @@
  */
 
 /// <summary>
-/// @author shack
+/// @author shack (.NET version by eja, the same holds for many files)
 /// </summary>
-
-// eja commented regex out, commented stack trace out so the build works again -- at least for now for testing, maybe forever ... it's unclear whether this functionality was ever used and/or working, TODO purge the feature or convert and comment in again
 
 namespace de.unika.ipd.grgen.util.report
 {
 
 	using System;
 	using System.Text;
+	using System.Diagnostics;
+	using System.Text.RegularExpressions;
 
 	/// <summary>
 	/// A debug message reporter.
 	/// </summary>
 	public class DebugReporter : Reporter
 	{
-		private void InitializeInstanceFields()
-		{
-			//matcher = pattern.Matcher("");
-		}
-
-		//private Pattern pattern = Pattern.compile(".*");
-		//private Matcher matcher;
+		private String pattern = ".*";
 		private bool inclusive = true;
-		//private bool includeClassName = false;
+		private bool includeClassName = false;
 
 		private string prefix = "";
 		private bool enableStackTrace = true;
 
 		public DebugReporter(int mask)
 		{
-			InitializeInstanceFields();
 			Mask = mask;
 		}
 
@@ -46,13 +39,12 @@ namespace de.unika.ipd.grgen.util.report
 		/// The class filter is a regular expression. Each class calling
 		/// this debug reporter is matched against this regex. Only if the
 		/// regex matches, the message is reported. </summary>
-		/// <param name="regex"> A regular expression. </param>
+		/// <param name="value"> A regular expression. </param>
 		public virtual string Filter
 		{
 			set
 			{
-				//pattern = Pattern.Compile(value);
-				//matcher = pattern.Matcher("");
+				pattern = value;
 			}
 		}
 
@@ -83,25 +75,21 @@ namespace de.unika.ipd.grgen.util.report
 		{
 			if(enableStackTrace)
 			{
-				/*StackTraceElement[] st = (new Exception()).GetStackTrace();
-				StackTraceElement ste = st[2];
+				StackFrame[] st = new StackTrace().GetFrames();
+				StackFrame sf = st[2];
 				StringBuilder sb = new StringBuilder();
 				for(int i = 0; i < st.Length; i++)
 					sb.Append(' ');
-				string className = ste.GetClassName();
-
-				int lastDot = className.LastIndexOf('.');
-				if(lastDot != -1)
-					className = className.Substring(lastDot + 1);
 
 				if(includeClassName)
 				{
+					string className = sf.GetMethod().DeclaringType.Name; // maybe runtime type would be preferable...
 					sb.Append(className);
 					sb.Append('.');
 				}
-				sb.Append(ste.GetMethodName());
-				prefix = sb.ToString();*/
-				prefix = "";
+
+				sb.Append(sf.GetMethod().Name);
+				prefix = sb.ToString();
 			}
 			else
 				prefix = "";
@@ -117,8 +105,8 @@ namespace de.unika.ipd.grgen.util.report
 
 			if(prefix.Length != 0)
 			{
-				//bool matches = matcher.Reset(prefix).Matches();
-				//res += matches ? 1 : 0;
+				bool matches = Regex.IsMatch(prefix, pattern);
+				res += matches ? 1 : 0;
 			}
 
 			return (res == 0 || res == 2) && base.WillReport(channel);
